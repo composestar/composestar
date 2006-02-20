8fc5959483dae0c73be15e076c27b1667dfc2362
Rolf Huisman <>
2006-02-20 08:14:56 +0000

    For easier development
    The old Make4Win to support long pathnames.
    Added core and core.net compilation
    Added pause for oneclick compile
    Modified make src in make so installs as well

---
+++ composestarruntimedotnet/make4win.bat
@@ -0,0 +1,34 @@
+ECHO OFF
+REM **************************************************************************
+REM Batch file to run make under windows
+REM
+REM The Windows command prompt is having difficulties with long filenames,
+REM i.e. if the Composestar source is in a long-named folder location it
+REM crashed the java compiler with file not found exceptions. Use this batch
+REM file to make a virtual mapping of the Composestar source folder to a
+REM drive letter and avoid the problem.
+REM **************************************************************************
+
+ECHO Mapping source root to virtual drive...
+
+cd..
+
+IF "%1"=="" (
+  SET DRIVE=q:
+) ELSE (
+	SET DRIVE=%1
+)
+
+SUBST %DRIVE% .
+IF %ERRORLEVEL% == 0 (
+	CMD /C "%DRIVE% && cd composestarcore && make && cd .. && cd composestardotnet && make && cd .. && cd composestarruntimedotnet && make && cd .."
+	IF not %ERRORLEVEL% == 0 (
+		pause
+	)
+	SUBST /D %DRIVE%
+) ELSE (
+	ECHO Please specify a free drive letter.
+	pause
+)
+cd composestarruntimedotnet
+
