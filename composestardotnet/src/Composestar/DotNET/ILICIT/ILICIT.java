4c2b89f2b3801011d212467520c9fdef14df525f
Dennis Spenkel <>
2006-03-08 09:29:34 +0000

    Weaver weaves on fresh copies of built assemblies Support for incremental compilation

--- composestardotnet/src/Composestar/DotNET/ILICIT/ILICIT.java
+++ composestardotnet/src/Composestar/DotNET/ILICIT/ILICIT.java
@@ -5,7 +5,7 @@
  * Licensed under LGPL v2.1 or (at your option) any later version.
  * [http://www.fsf.org/copyleft/lgpl.html]
  *
- * $Id: ILICIT.java,v 1.1 2006/02/16 23:10:57 pascal_durr Exp $
+ * $Id: ILICIT.java,v 1.2 2006/02/24 15:56:00 dspenkel Exp $
  */
 
 
@@ -16,7 +16,7 @@ package Composestar.DotNET.ILICIT;
  * Licensed under LGPL v2.1 or (at your option) any later version.
  * [http://www.fsf.org/copyleft/lgpl.html]
  *
- * $Id: ILICIT.java,v 1.1 2006/02/16 23:10:57 pascal_durr Exp $
+ * $Id: ILICIT.java,v 1.2 2006/02/24 15:56:00 dspenkel Exp $
  */
 
 import java.io.BufferedWriter;
@@ -29,129 +29,344 @@ import java.util.Properties;
 import Composestar.Core.WEAVER.*;
 import Composestar.Utils.CommandLineExecutor;
 import Composestar.Utils.Debug;
+import Composestar.Utils.FileUtils;
 import Composestar.Utils.StringConverter;
+import Composestar.Core.CpsProgramRepository.Concern;
+import Composestar.Core.CpsProgramRepository.PrimitiveConcern;
+import Composestar.Core.CpsProgramRepository.CpsConcern.CpsConcern;
+import Composestar.Core.CpsProgramRepository.CpsConcern.Filtermodules.FilterModule;
+import Composestar.Core.CpsProgramRepository.CpsConcern.Filtermodules.Internal;
+import Composestar.Core.CpsProgramRepository.CpsConcern.Implementation.CompiledImplementation;
 import Composestar.Core.Exception.ModuleException;
+import Composestar.Core.FILTH.FilterModuleOrder;
+import Composestar.Core.INCRE.INCRE;
 import Composestar.Core.Master.CommonResources;
 import Composestar.Core.RepositoryImplementation.DataStore;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ILICIT implements WEAVER {
 
-	public static final String version = "$Revision: 1.1 $";
+	public static final String version = "$Revision: 1.2 $";
 	
     public void run(CommonResources resources) throws ModuleException {
-      PrintWriter out = null;
-	  ArrayList builtAssemblies = (ArrayList) resources.getResource("BuiltAssemblies");
-	  
-	  if(!builtAssemblies.isEmpty())
-	  {
-	  	String[] assemblyPaths = (String[]) builtAssemblies.toArray(new String[builtAssemblies.size()]);
-	  	String binPath = resources.ProjectConfiguration.getProperty("ComposestarPath");
-	  	String tempPath = resources.ProjectConfiguration.getProperty("TempFolder");
-	  	String buildPath = resources.ProjectConfiguration.getProperty("BuildPath");
-	  	String peweaver = binPath + "binaries\\peweaver.exe";
-	  	String weavefile = "\"" + tempPath + "weavespec.xml" + '\"';
-	  	String targets = "";
-	  	if ( assemblyPaths.length > 20 ) {
-	  		try {
-	  			out = new PrintWriter(new BufferedWriter(new FileWriter(tempPath + "filelist.peweaver")));
+     PrintWriter out = null; 
+     String binPath = resources.ProjectConfiguration.getProperty("ComposestarPath");
+	 String tempPath = resources.ProjectConfiguration.getProperty("TempFolder");
+	 String buildPath = resources.ProjectConfiguration.getProperty("BuildPath");
+	 String weavePath = buildPath + "Weaver\\";
+	 String peweaver = binPath + "binaries\\peweaver.exe";
+	 String weavefile = "\"" + tempPath + "weavespec.xml" + '\"';
+	      
+     ArrayList compiledSources = (ArrayList) resources.getResource("CompiledSources");
+	 ArrayList builtAssemblies = new ArrayList();
+	 ArrayList toBeWeaved = new ArrayList();
+	 
+	 // make a copy of the built binaries 
+	 Iterator binItr = compiledSources.iterator();
+	 File weaveDir = new File(weavePath);
+	 if(!weaveDir.exists()) weaveDir.mkdir(); /* create directory */
+	 while(binItr.hasNext()){
+		 String asm = (String)binItr.next();
+		 File source = new File(asm);
+		 String target = FileUtils.fixFilename(weavePath+source.getName());
+		 File ftarget = new File(target);
+		 if(!ftarget.exists() /* add to config.xml */ || !INCRE.instance().isProcessedByModule(asm,"ILICIT")){
+			 Debug.out(Debug.MODE_DEBUG,"ILICIT","copying "+asm+" to Weaver directory...");
+			 FileUtils.copyFile(target,source.getAbsolutePath());	 
+			 builtAssemblies.add(target);
+			 toBeWeaved.add(target);
+		 }
+		 else {
+			 // no need to weave the file
+			 Debug.out(Debug.MODE_DEBUG,"INCRE","No need to re-weave "+asm);
+			 builtAssemblies.add(target);
+		 }
+	 }
+	 resources.addResource("BuiltAssemblies", builtAssemblies);
+	 
+	 //also copy dummies 
+	 String assemblyList = resources.ProjectConfiguration.getProperty("Assemblies");
+	 String[] asmPaths = assemblyList.split(",");
+	 for( int i = 0; i < asmPaths.length; i++ ) 
+	 {  
+			String asm = asmPaths[i];
+			File asmFile = new File(asm);
+			Debug.out(Debug.MODE_DEBUG,"ILICIT","copying "+asm+" to Weaver directory...");
+			FileUtils.copyFile(FileUtils.fixFilename(weavePath+asmFile.getName()),asm);
+	 }
+	 
+	 String[] assemblyPaths = (String[]) toBeWeaved.toArray(new String[toBeWeaved.size()]);
+	 String targets = "";
+	 if(assemblyPaths.length > 0 )
+	 {
+	  if ( assemblyPaths.length > 20 ) {
+	 	try {
+	 		out = new PrintWriter(new BufferedWriter(new FileWriter(tempPath + "filelist.peweaver")));
 	  		
-	  			for( int i = 0; i < assemblyPaths.length; i++ )
-	  			{
-	  				out.println(assemblyPaths[i].replaceAll("\"", ""));
-	  			}
+	 		for( int i = 0; i < assemblyPaths.length; i++ )
+	 		{
+	 			out.println(assemblyPaths[i].replaceAll("\"", ""));
+	 		}
 			
-	  			out.flush();
-	  			out.close();
-	  		}
-	  		catch (IOException e) {
-	  			throw new ModuleException("Unable to create the build file for the weaver.","CONE_IS");
-	  		}
-	  	}
-	  	else {
-	  		targets = StringConverter.stringListToString(assemblyPaths, " ");
-	  	}
+	 		out.flush();
+	 		out.close();
+	 	}
+	 	catch (IOException e) {
+	 		throw new ModuleException("Unable to create the build file for the weaver.","CONE_IS");
+	 	}
+	  } 
+	  else {
+	 	targets = StringConverter.stringListToString(assemblyPaths, " ");
+	  }
+    
+	  // verify assemblies?
+	  boolean verify = false;
+      if (resources.ProjectConfiguration.getProperty("VerifyAssemblies") != null) {
+	 	if ("yes".equalsIgnoreCase(resources.ProjectConfiguration.getProperty("VerifyAssemblies"))) {
+	 		verify = true;
+	 	}
+	  }
       
-	  	boolean verify = false;
-      
-	  	if (resources.ProjectConfiguration.getProperty("VerifyAssemblies") != null) {
-	  		if ("yes".equalsIgnoreCase(resources.ProjectConfiguration.getProperty("VerifyAssemblies"))) {
-	  			verify = true;
-	  		}
-	  	}
-      
-	  	CommandLineExecutor cle = new CommandLineExecutor();
+	  CommandLineExecutor cle = new CommandLineExecutor();
 
-	  	File f = new File(peweaver);
-	  	if (!f.exists()) {
-	  		throw new ModuleException("Unable to locate the executable '" + peweaver + "'!");
-	  	}
-	  	else {
-	  		String args = " /nologo";
+	  File f = new File(peweaver);
+	  if (!f.exists()) {
+	 	throw new ModuleException("Unable to locate the executable '" + peweaver + "'!");
+	  }
+	  else {
+	 	String args = " /nologo";
         
-	  		if (verify) {
-	  			args = args.concat(" /verify");
-	  		}
+	 	if (verify) {
+	 		args = args.concat(" /verify");
+	 	}
         
-	  		// If debugging supply the /debug switch
-	  		if ( Debug.getMode() == Debug.MODE_DEBUG ) args = args.concat(" /debug");
+	 	// If debugging supply the /debug switch
+	 	if ( Debug.getMode() == Debug.MODE_DEBUG ) args = args.concat(" /debug");
         
-	  		args = args.concat(" /ws=" + weavefile);
+	 	args = args.concat(" /ws=" + weavefile);
 
-	  		if ( targets.equals("") ) {
-	  			args = args.concat(" /filelist=\"" + tempPath + "filelist.peweaver\"");
-	  		}
-	  		else {
-	  			args = args.concat(" " + targets);
-	  		}
+	 	if ( targets.equals("") ) {
+	 		args = args.concat(" /filelist=\"" + tempPath + "filelist.peweaver\"");
+	 	}
+	 	else {
+	 		args = args.concat(" " + targets);
+	 	}
         
-	  		String cmd = "call \"" + peweaver + "\" " + args;
+	 	String cmd = "call \"" + peweaver + "\" " + args;
 		
-	  		// If debugging write output from PeWeaver to log file
-	  		//if ( Debug.getMode() == Debug.MODE_DEBUG ) 
-	  			cmd += " > \"" + buildPath + "peweaver.log\"";
+		// If debugging write output from PeWeaver to log file
+		if ( Debug.getMode() == Debug.MODE_DEBUG ) 
+			cmd += " > \"" + weavePath + "peweaver.log\"";
 		
-	  			Debug.out(Debug.MODE_DEBUG, "ILICIT", "Starting execution of the 'PE Weaver' tool with arguments '" + args + "'...");
-	  			int exitcode = cle.exec(cmd);
+			Debug.out(Debug.MODE_DEBUG, "ILICIT", "Starting execution of the 'PE Weaver' tool with arguments '" + args + "'...");
+			int exitcode = cle.exec(cmd);
 
-	  			if (exitcode != 0) {
-	  				Debug.out(Debug.MODE_DEBUG, "ILICIT", cle.outputNormal());
-	  			}
+			if (exitcode != 0) {
+				Debug.out(Debug.MODE_DEBUG, "ILICIT", cle.outputNormal());
+			}
         
-	  			String msg = "";
+	  		String msg = "";
         
-	  			switch(exitcode)
-				{
-				case 1: msg = "General PEWeaver failure"; break;
-				case 2: msg = "Unable to find weave specification file"; break;
-				case 3: msg = "An error occured in the weaving process"; break;
-				case 4: msg = "Missing ildasm executable (ildasm.exe)"; break;
-				case 5: msg = "Missing ilasm executable (SDK tool ilasm.exe)"; break;
-				case 6: msg = "Missing PeVerify executable (SDK tool peverify.exe)"; break;
-				case 9: msg = "Input file not found"; break;
-				case 10: msg = "IL disassembler not found (ildasm.exe)"; break;
-				case 11: msg = "IL disassembler execution failure"; break;
-				case 13: msg = "IL file not found"; break;
-				case 14: msg = "Unsupported file format"; break;
-				case 15: msg = "IL assemmbler not found (ilasm.exe)"; break;
-				case 16: msg = "IL assembler execution failure"; break;
-				case 17: msg = "Output file not found"; break;
-				case 18: msg = "PEVerify tool not found (SDK tool peverify.exe)"; break;
-				case 19: msg = "PEVerify execution failure"; break;
-				case 25: msg = "ILWeaver not found (ilweaver.exe)"; break;
-				case 26: msg = "ILWeaver execution failure"; break;
-				default: msg = "PeWeaver execution failure (exitcode " + exitcode + ')'; break;
+	  		switch(exitcode)
+			{
+			case 1: msg = "General PEWeaver failure"; break;
+			case 2: msg = "Unable to find weave specification file"; break;
+			case 3: msg = "An error occured in the weaving process"; break;
+			case 4: msg = "Missing ildasm executable (ildasm.exe)"; break;
+			case 5: msg = "Missing ilasm executable (SDK tool ilasm.exe)"; break;
+			case 6: msg = "Missing PeVerify executable (SDK tool peverify.exe)"; break;
+			case 9: msg = "Input file not found"; break;
+			case 10: msg = "IL disassembler not found (ildasm.exe)"; break;
+			case 11: msg = "IL disassembler execution failure"; break;
+			case 13: msg = "IL file not found"; break;
+			case 14: msg = "Unsupported file format"; break;
+			case 15: msg = "IL assemmbler not found (ilasm.exe)"; break;
+			case 16: msg = "IL assembler execution failure"; break;
+			case 17: msg = "Output file not found"; break;
+			case 18: msg = "PEVerify tool not found (SDK tool peverify.exe)"; break;
+			case 19: msg = "PEVerify execution failure"; break;
+			case 25: msg = "ILWeaver not found (ilweaver.exe)"; break;
+			case 26: msg = "ILWeaver execution failure"; break;
+			default: msg = "PeWeaver execution failure (exitcode " + exitcode + ')'; break;
+			}
+
+	  		if (exitcode == 0)
+	  			Debug.out(Debug.MODE_DEBUG, "ILICIT", "Successfully executed the 'PE Weaver' tool.");
+	  		else
+	  			throw new ModuleException(msg, "ILICIT");        
+	   }
+	  }//end assemblyPaths > 0
+	}
+
+    /**
+	 * @param src Absolute path of a sourcefile 
+	 * @return ArrayList containing all concerns with FMO and extracted from 
+	 * the source and its external linked sources
+	 */
+	public ArrayList getConcernsWithFMO(String src)
+	{
+		INCRE incre = INCRE.instance();
+		ArrayList concerns = new ArrayList();
+		ArrayList concernsWithFMO = incre.getConcernsWithFMO();
+		ArrayList sources = (ArrayList)incre.externalSourcesBySource.get(FileUtils.removeExtension(src));
+		sources.add(0,src);
+		
+		if(!concernsWithFMO.isEmpty()){
+			Iterator iterConcerns = concernsWithFMO.iterator();
+
+			while( iterConcerns.hasNext() )
+			{
+				Concern c = (Concern)iterConcerns.next();
+				if(incre.declaredInSources(c,sources)){
+					concerns.add(c.getQualifiedName());
 				}
+			}
+		}
+		
+		return concerns;
+	}
+	
+	/**
+	 * @param src Absolute path of a sourcefile 
+	 * @return ArrayList containing all concerns recognized as a casting interception 
+	 * Only concerns extracted from the source and its external linked sources are returned
+	 */
+	public ArrayList castingInterceptions(String src) throws ModuleException
+	{
+		ArrayList list = new ArrayList();
+     	INCRE incre = INCRE.instance();
+     	DataStore ds = incre.getCurrentRepository();
+     	ArrayList concernsWithFMO = incre.getConcernsWithFMO();
+		ArrayList sources = (ArrayList)incre.externalSourcesBySource.get(FileUtils.removeExtension(src));
+		sources.add(0,src);
+     	
+		if(!concernsWithFMO.isEmpty())
+		{
+     		Iterator iterConcerns = concernsWithFMO.iterator();
+			while ( iterConcerns.hasNext() )
+			{
+				Concern c = (Concern)iterConcerns.next();
+				boolean castConcern = false;
+				
+				if(incre.declaredInSources(c,sources))
+				{
+					FilterModuleOrder fmo = (FilterModuleOrder)c.getDynObject("SingleOrder");
+			
+					Iterator iterFilterModules = fmo.orderAsList().iterator();
+					while ( iterFilterModules.hasNext() )
+					{
+						String fmref = (String)iterFilterModules.next();
+						FilterModule fm = (FilterModule) ds.getObjectByID(fmref);
+				  	  			
+						Iterator iterInternals = fm.getInternalIterator() ;
+						while ( iterInternals.hasNext() )
+						{
+							Internal internal = (Internal)iterInternals.next();
+							if ( !list.contains(internal.type.getQualifiedName()) )
+							{
+								castConcern = true;
+								list.add( internal.type.getQualifiedName() );
+							}
+						}
+					}
+								  	  		
+					if ( castConcern ) 
+					{
+						if(!list.contains( c.getQualifiedName() ))
+							list.add( c.getQualifiedName() );
+					}
+				}
+			}
+		}
+		
+		return list;
+	}
 
-	  			if (exitcode == 0)
-	  				Debug.out(Debug.MODE_DEBUG, "ILICIT", "Successfully executed the 'PE Weaver' tool.");
-	  			else
-	  				throw new ModuleException(msg, "ILICIT");        
-	  	}
-	  }	
-    }
+	/**
+	 * @param src Absolute path of a sourcefile 
+	 * @return ArrayList containing all concerns which instantiation should be intercepted 
+	 * Only concerns extracted from the source and its external linked sources are returned
+	 */
+	public ArrayList getAfterInstantiationClasses(String src) throws ModuleException 
+	{
+		INCRE incre = INCRE.instance();
+		ArrayList result = new ArrayList();
+		ArrayList sources = (ArrayList)incre.externalSourcesBySource.get(FileUtils.removeExtension(src));
+		sources.add(0,src);
+		
+		Iterator it = incre.getAllInstancesOfOrdered(CompiledImplementation.class);
+		while(it.hasNext())
+		{
+			CompiledImplementation ci = (CompiledImplementation)it.next();
+			String className = ci.getClassName();
+			if(className != null)
+				result.add(className);
+		}
+		
+		it = incre.getAllInstancesOfOrdered(CpsConcern.class);
+		while (it.hasNext()) {
+			CpsConcern c = (CpsConcern)it.next();
+			Object o = c.getDynObject("IMPLEMENTATION");
+			if ( o != null) {
+				
+				PrimitiveConcern pc = (PrimitiveConcern)o;
+				result.add(pc.getQualifiedName());
+			}
+		}
+		
+		it = incre.getAllInstancesOfOrdered(Concern.class);
+		while (it.hasNext()) {
+			Concern c = (Concern)it.next();
+			if(incre.declaredInSources(c,sources)){
+				if(c.getDynObject("superImpInfo") != null && !(c instanceof CpsConcern))
+				{
+						result.add(c.getQualifiedName());
+				}
+			}
+		}
+			
+		return result;
+	}
 
+	/**
+	 * @param src Absolute path of a sourcefile 
+	 * @return ArrayList containing all concerns with outputfilter(s) 
+	 * Only concerns extracted from the source and its external linked sources are returned
+	 */
+	public ArrayList getConcernsWithOutputFilters(String src) throws ModuleException
+	{
+		ArrayList concerns = new ArrayList();
+		INCRE incre = INCRE.instance();
+		DataStore ds = incre.getCurrentRepository();
+		ArrayList concernsWithFMO = incre.getConcernsWithFMO();
+		ArrayList sources = (ArrayList)incre.externalSourcesBySource.get(FileUtils.removeExtension(src));
+		sources.add(0,src);
+		
+		if(!concernsWithFMO.isEmpty()){
+     	Iterator iterConcerns = concernsWithFMO.iterator();
+			while ( iterConcerns.hasNext() )
+			{
+				Concern c = (Concern)iterConcerns.next();
+				if(incre.declaredInSources(c,sources)){
+					FilterModuleOrder fmo = (FilterModuleOrder)c.getDynObject("SingleOrder");
+			
+					Iterator iterFilterModules = fmo.orderAsList().iterator();
+					while ( iterFilterModules.hasNext() )
+					{
+						FilterModule fm = (FilterModule) ds.getObjectByID((String)iterFilterModules.next());
+			
+						if ( !fm.outputFilters.isEmpty() )
+							concerns.add( c.getQualifiedName() );
+					}
+				}
+			} 
+		}
+
+		return concerns;
+	}
+	
     public void main(String[] args) {
       CommonResources resources = new CommonResources();
       DataStore ds = DataStore.instance();
