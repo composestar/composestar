38b2ba0d833905eec88bec296394c4d5cce2d16a
W.K. Havinga <>
2006-03-16 14:50:51 +0000

    Parser-based dummy emitter for CSharp (using an external program written in C#)

---
+++ composestardotnet/src/Composestar/DotNET/DUMMER/CSharpDummyEmitter.java
@@ -0,0 +1,90 @@
+package Composestar.DotNET.DUMMER;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Collection;
+import java.util.Iterator;
+
+import Composestar.Core.DUMMER.DummyEmitter;
+import Composestar.Core.Exception.ModuleException;
+import Composestar.Core.Master.Config.Configuration;
+import Composestar.Core.Master.Config.Source;
+
+public class CSharpDummyEmitter implements DummyEmitter {
+		
+	public void createDummy(Source source, String outputFilename) throws ModuleException
+	{
+		try
+		{
+			CSharpDummyProcess dummyGen = new CSharpDummyProcess();
+			dummyGen.openProcess();
+			dummyGen.getStdout().println(source.getFileName());
+			dummyGen.getStdout().println(outputFilename);
+			dummyGen.getStdout().close();
+			dummyGen.getProcess().waitFor();
+		}
+		catch (Exception e)
+		{
+			throw new ModuleException("DUMMER", e.getCause().getMessage());
+		}
+	}
+
+	public void createDummies(Collection sources, Collection outputFilenames) throws ModuleException
+	{
+		try
+		{
+			CSharpDummyProcess dummyGen = new CSharpDummyProcess();
+			dummyGen.openProcess();
+            
+            Iterator srcIter = sources.iterator();
+            Iterator outputIter = outputFilenames.iterator();
+            while (srcIter.hasNext())
+            {
+            	dummyGen.getStdout().println(((Source)srcIter.next()).getFileName());
+            	dummyGen.getStdout().println((String)outputIter.next());
+            }
+            dummyGen.getStdout().close();
+            dummyGen.getProcess().waitFor();
+		}
+		catch (Exception e)
+		{
+			throw new ModuleException(e.getCause().getMessage(), "DUMMER");
+		}
+	}
+}
+
+class CSharpDummyProcess
+{
+	private PrintStream stdout;
+	private InputStream stdin;
+	private Process process;
+	
+	public CSharpDummyProcess()
+	{
+	}
+	
+	public void openProcess() throws Exception
+	{
+		Configuration config = Configuration.instance();
+		String csDummyEmitter = config.getPathSettings().getPath("Composestar") + "binaries/CSharpDummygen.exe";
+
+        Runtime rt = Runtime.getRuntime();
+
+        Process proc = rt.exec(csDummyEmitter);
+
+        this.stdin = proc.getInputStream();
+        this.stdout = new PrintStream(proc.getOutputStream());		
+	}
+
+	public PrintStream getStdout() {
+		return stdout;
+	}
+
+	public InputStream getStdin() {
+		return stdin;
+	}
+
+	public Process getProcess() {
+		return process;
+	}
+}
