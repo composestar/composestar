b5202704bfa234cfa27ceb26dfa5a54d6811ae9c
W.K. Havinga <>
2006-03-16 14:43:44 +0000

    Some shuffling around of dummy generation functionality, to better facilitate this for both Java and .NET

--- composestarcore/src/Composestar/Core/DUMMER/DummyManager.java
+++ composestarcore/src/Composestar/Core/DUMMER/DummyManager.java
@@ -1,15 +1,17 @@
 package Composestar.Core.DUMMER;
 
-import Composestar.Core.COMP.LangCompiler;
-import Composestar.Core.Exception.*;
-import Composestar.Core.Master.*;
-import Composestar.Core.Master.CTCommonModule;
-import Composestar.Core.Master.Config.*;
-import Composestar.Utils.FileUtils;
-
-import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Vector;
+
+import Composestar.Core.COMP.LangCompiler;
+import Composestar.Core.Exception.ModuleException;
+import Composestar.Core.Master.CTCommonModule;
+import Composestar.Core.Master.CommonResources;
+import Composestar.Core.Master.Config.Configuration;
+import Composestar.Core.Master.Config.Project;
+import Composestar.Core.Master.Config.Source;
+import Composestar.Utils.FileUtils;
 
 public class DummyManager implements CTCommonModule {
 
@@ -27,42 +29,31 @@ public class DummyManager implements CTCommonModule {
 		
 		while( projIt.hasNext() ) {
 			Project p = (Project)projIt.next();
+			DummyEmitter emitter = p.getLanguage().getEmitter();
 			ArrayList sources = p.getSources();
+			Vector outputFilenames = new Vector(sources.size());
+			
 			Iterator sourceIt = sources.iterator();
-			while( sourceIt.hasNext() ) {
-				
-				DummyEmitter emitter = p.getLanguage().getEmitter();
+			while( sourceIt.hasNext() ) {				
 				
 				//create target of source 
 				Source source = (Source)sourceIt.next();
 				String dummyPath = config.getPathSettings().getPath("Dummy");
-				String target = "";
 				try {
-					target = FileUtils.createOutputFilename(p.getProperty("basePath"),"obj/"+dummyPath,source.getFileName());
+					String target = FileUtils.createOutputFilename(p.getProperty("basePath"),"obj/"+dummyPath,source.getFileName());
+					String targetPath = FileUtils.getDirectoryPart(target);
+					FileUtils.createFullPath(targetPath); // Make sure the directory exists
+
+					outputFilenames.add(target);
+					source.setDummy(target);
 				}
 				catch(Exception e) {
 					throw new ModuleException("Error while creating targetfile of dummy: "+e.getMessage(),"DUMMER");
 				}
-								
-				String targetPath = target.substring(0,target.lastIndexOf("/")+1);
-				File fileDir = new File(targetPath);
-				BufferedWriter bw;		
-				//create dir(s)
-				try {
-					if(!fileDir.exists())	{
-						fileDir.mkdirs();
-					}
-					bw = new BufferedWriter(new FileWriter(target));
-				}
-				catch(IOException io) {
-					throw new ModuleException("ERROR while creating dummy directory: "+io.getMessage(),"DUMMER");
-				}
-				//create dummy
-				emitter.createDummy(source,bw);
-
-				//add dummy to source
-				source.setDummy(target);
+				//emitter.createDummy(source,target);
 			}
+			// Create all the dummies in one go.
+			emitter.createDummies(sources, outputFilenames);
 			
 			/** compile dummies */
 			LangCompiler comp = (LangCompiler)p.getLanguage().compilerSettings.getCompiler();
