9257468587cc64bc5610eb25daa2644b123f28cf
W.K. Havinga <>
2006-03-16 14:40:33 +0000

    Interface has to change...my mistake :P

--- composestarcore/src/Composestar/Core/DUMMER/DummyEmitter.java
+++ composestarcore/src/Composestar/Core/DUMMER/DummyEmitter.java
@@ -1,12 +1,15 @@
 package Composestar.Core.DUMMER;
 
+import java.util.Collection;
+
 import Composestar.Core.Exception.ModuleException;
 import Composestar.Core.Master.Config.Source;
 
-import java.io.*;
-
 public interface DummyEmitter {
 	
-	public abstract void createDummy(Source source, BufferedWriter bw) throws ModuleException;  
+	//public abstract void createDummy(Source source, BufferedWriter bw) throws ModuleException;  
+	public abstract void createDummy(Source source, String outputFilename) throws ModuleException;
+	
+	public abstract void createDummies(Collection sources, Collection outputFilenames) throws ModuleException;
 	
 }
