cb788a80459b12ab49bf775c9e50a689059bbdfa
Michiel van Oudheusden <mivano@users.noreply.github.com>
2006-06-26 13:19:47 +0000

    added files for semtex

---
+++ src/Composestar/Core/LAMA/ResourceUsage.java
@@ -0,0 +1,63 @@
+/*
+ * Created on 26-apr-2006
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package Composestar.Core.LAMA;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import Composestar.Core.RepositoryImplementation.SerializableRepositoryEntity;
+
+/**
+ * @author Michiel van Oudheusden
+ *
+ */
+public class ResourceUsage implements SerializableRepositoryEntity {
+	
+	public String ResourceName;
+	public String AccessType;       
+	public String AccessOccurence;
+	public Type Parent;
+	
+	public ResourceUsage()
+	{
+		
+	}
+	
+	public Type parent() 
+	{
+		return Parent;
+	}
+	
+	public void setParent(Type parent) 
+	{
+	    Parent = parent;
+	}
+	
+		
+	/**
+	 * Custom deserialization of this object
+	 */
+	private void readObject(ObjectInputStream in) throws IOException,ClassNotFoundException
+	{
+		ResourceName = in.readUTF();
+		Parent = (Type)in.readObject();
+		AccessType = in.readUTF();
+		AccessOccurence = in.readUTF();
+	}
+	
+	/**
+	 * Custom serialization of this object
+	 */
+	private void writeObject(ObjectOutputStream out) throws IOException
+	{
+		out.writeUTF(ResourceName);
+		out.writeObject(Parent);
+		out.writeUTF(AccessType);
+		out.writeUTF(AccessOccurence);
+	}
+}
