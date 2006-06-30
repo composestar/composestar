8663f443405d85ad35b6ba224f32eca10f135927
W.K. Havinga <>
2006-06-30 12:32:43 +0000

    Not much abstractness about this class?

--- src/Composestar/Core/LAMA/Annotation.java
+++ src/Composestar/Core/LAMA/Annotation.java
@@ -5,7 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-public abstract class Annotation implements Serializable{
+public class Annotation implements Serializable{
 
 	public Type type;
 	public String value;
