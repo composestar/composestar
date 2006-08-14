28ae316d1c51af15fac9f8281c5e5070ad99e872
Roy Spenkelink <>
2006-08-14 09:14:24 +0000

    - Added custom (de)serialization, necessary for serialization of the repository

--- composestarjava/src/Composestar/Java/LAMA/JavaFieldInfo.java
+++ composestarjava/src/Composestar/Java/LAMA/JavaFieldInfo.java
@@ -4,24 +4,30 @@ import Composestar.Core.LAMA.FieldInfo;
 import Composestar.Core.LAMA.UnitResult;
 import Composestar.Utils.StringConverter;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 
-public class JavaFieldInfo extends FieldInfo {
+public class JavaFieldInfo extends FieldInfo 
+{
 
 	private static final long serialVersionUID = -3329314904670932060L;
 	
 	public Field theField;
 	
-	public JavaFieldInfo( Field f ) {
+	public JavaFieldInfo( Field f ) 
+	{
 		super();
 		this.theField = f;
 	}
 	
-	public boolean isDeclaredHere() {
+	public boolean isDeclaredHere() 
+	{
 		if((Parent.fullName()).equals(theField.getDeclaringClass().getName()))
 			return true;
 		return false;
@@ -30,38 +36,58 @@ public class JavaFieldInfo extends FieldInfo {
 	/** Stuff for LOLA **/
 	
 	/* (non-Javadoc)
-     * @see Composestar.core.LAMA.ProgramElement#getUnitAttributes()
-     */
-    public Collection getUnitAttributes() {
-    	HashSet result = new HashSet();
-    	Iterator modifierIt = StringConverter.stringToStringList( Modifier.toString(theField.getModifiers()), " " );
-		while(modifierIt.hasNext()){
+	 * @see Composestar.core.LAMA.ProgramElement#getUnitAttributes()
+	 */
+	public Collection getUnitAttributes() 
+	{
+		HashSet result = new HashSet();
+		Iterator modifierIt = StringConverter.stringToStringList( Modifier.toString(theField.getModifiers()), " " );
+		while(modifierIt.hasNext())
+		{
 			result.add(modifierIt.next());
 		}
 		return result;
-    }
+	}
     
 	/* (non-Javadoc)
-	   * @see Composestar.Core.LAMA.ProgramElement#getUnitRelation(java.lang.String)
-	   */
-	public UnitResult getUnitRelation(String argumentName) {
+	 * @see Composestar.Core.LAMA.ProgramElement#getUnitRelation(java.lang.String)
+	 */
+	public UnitResult getUnitRelation(String argumentName) 
+	{
 	    
 		if ("ParentType".equals(argumentName))
-	      return new UnitResult(Parent);
-	    else if ("Class".equals(argumentName) && "Class".equals(fieldType().getUnitType()))
-	      return new UnitResult(fieldType());
-	    else if ("Interface".equals(argumentName) && "Interface".equals(fieldType().getUnitType()))
-	      return new UnitResult(fieldType());
-	    else if ("Annotation".equals(argumentName) && "Annotation".equals(fieldType().getUnitType()))
-	        return new UnitResult(fieldType());
-	    else if ("Annotations".equals(argumentName)) {
-	      Iterator i = getAnnotations().iterator();
-	      HashSet res = new HashSet();
-	      while (i.hasNext())
-	        res.add(((JavaAnnotation)i.next()).getType());
-	      return new UnitResult(res);
-	    }
+			return new UnitResult(Parent);
+		else if ("Class".equals(argumentName) && "Class".equals(fieldType().getUnitType()))
+			return new UnitResult(fieldType());
+		else if ("Interface".equals(argumentName) && "Interface".equals(fieldType().getUnitType()))
+			return new UnitResult(fieldType());
+		else if ("Annotation".equals(argumentName) && "Annotation".equals(fieldType().getUnitType()))
+			return new UnitResult(fieldType());
+		else if ("Annotations".equals(argumentName)) 
+		{
+			Iterator i = getAnnotations().iterator();
+			HashSet res = new HashSet();
+			while (i.hasNext())
+				res.add(((JavaAnnotation)i.next()).getType());
+			return new UnitResult(res);
+		}
 	    
-	    return null;
+		return null;
+	}
+	
+	/**
+	 * Custom deserialization of this object
+	 */
+	private void readObject(ObjectInputStream in) throws IOException,ClassNotFoundException
+	{
+		// Nothing yet
+	}
+	
+	/**
+	 * Custom serialization of this object
+	 */
+	private void writeObject(ObjectOutputStream out) throws IOException
+	{
+		// Nothing yet
 	}
 }
--- composestarjava/src/Composestar/Java/LAMA/JavaMethodInfo.java
+++ composestarjava/src/Composestar/Java/LAMA/JavaMethodInfo.java
@@ -4,24 +4,30 @@ import Composestar.Core.LAMA.MethodInfo;
 import Composestar.Core.LAMA.UnitResult;
 import Composestar.Utils.StringConverter;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 
-public class JavaMethodInfo extends MethodInfo {
+public class JavaMethodInfo extends MethodInfo 
+{
 
 	private static final long serialVersionUID = 308590098089260675L;
 	
 	public Method theMethod;
 	
-	public JavaMethodInfo(Method m) {
+	public JavaMethodInfo(Method m) 
+	{
 		super();
 		this.theMethod = m;
 	}
 	
-	public boolean isDeclaredHere() {
+	public boolean isDeclaredHere() 
+	{
 		if((Parent.fullName()).equals(theMethod.getDeclaringClass().getName()))
 			return true;
 		return false;
@@ -29,50 +35,72 @@ public class JavaMethodInfo extends MethodInfo {
 	
 	/** Stuff for LOLA **/
 	
-	private HashSet toHashSet(Collection c) {
-      HashSet result = new HashSet();
-      Iterator iter = c.iterator();
-      while (iter.hasNext())
-        result.add(iter.next());
-      return result;
-    }
+	private HashSet toHashSet(Collection c) 
+	{
+		HashSet result = new HashSet();
+		Iterator iter = c.iterator();
+		while (iter.hasNext())
+			result.add(iter.next());
+		return result;
+	}
 	
 	/* (non-Javadoc)
-     * @see Composestar.core.LAMA.ProgramElement#getUnitAttributes()
-     */
-    public Collection getUnitAttributes() {
-    	HashSet result = new HashSet();
-    	Iterator modifierIt = StringConverter.stringToStringList( Modifier.toString(theMethod.getModifiers()), " " );
-		while(modifierIt.hasNext()){
+	 * @see Composestar.core.LAMA.ProgramElement#getUnitAttributes()
+	 */
+	public Collection getUnitAttributes() 
+	{
+		HashSet result = new HashSet();
+		Iterator modifierIt = StringConverter.stringToStringList( Modifier.toString(theMethod.getModifiers()), " " );
+		while(modifierIt.hasNext())
+		{
 			result.add(modifierIt.next());
 		}
 		return result;
-    }
+	}
     
-    /* (non-Javadoc)
-     * @see Composestar.Core.LAMA.ProgramElement#getUnitRelation(java.lang.String)
-     */
-    public UnitResult getUnitRelation(String argumentName) {
-      if (argumentName.equals("ParentClass") && Parent.getUnitType().equals("Class"))
-        return new UnitResult(Parent);
-      else if (argumentName.equals("ParentInterface") && Parent.getUnitType().equals("Interface"))
-        return new UnitResult(Parent);      
-      else if (argumentName.equals("ChildParameters"))
-        return new UnitResult(toHashSet(Parameters));
-      else if (argumentName.equals("ReturnClass") && returnType().getUnitType().equals("Class"))
-        return new UnitResult(returnType());
-      else if (argumentName.equals("ReturnInterface") && returnType().getUnitType().equals("Interface"))
-        return new UnitResult(returnType());
-      else if (argumentName.equals("ReturnAnnotation") && returnType().getUnitType().equals("Annotation"))
-          return new UnitResult(returnType());
-      else if (argumentName.equals("Annotations")) {
-        Iterator i = getAnnotations().iterator();
-        HashSet res = new HashSet();
-        while (i.hasNext())
-          res.add(((JavaAnnotation)i.next()).getType());
-        return new UnitResult(res);
-      }        
+	/* (non-Javadoc)
+	 * @see Composestar.Core.LAMA.ProgramElement#getUnitRelation(java.lang.String)
+	 */
+	public UnitResult getUnitRelation(String argumentName) 
+	{
+		if (argumentName.equals("ParentClass") && Parent.getUnitType().equals("Class"))
+			return new UnitResult(Parent);
+		else if (argumentName.equals("ParentInterface") && Parent.getUnitType().equals("Interface"))
+			return new UnitResult(Parent);      
+		else if (argumentName.equals("ChildParameters"))
+			return new UnitResult(toHashSet(Parameters));
+		else if (argumentName.equals("ReturnClass") && returnType().getUnitType().equals("Class"))
+			return new UnitResult(returnType());
+		else if (argumentName.equals("ReturnInterface") && returnType().getUnitType().equals("Interface"))
+			return new UnitResult(returnType());
+		else if (argumentName.equals("ReturnAnnotation") && returnType().getUnitType().equals("Annotation"))
+			return new UnitResult(returnType());
+		else if (argumentName.equals("Annotations")) 
+		{
+			Iterator i = getAnnotations().iterator();
+			HashSet res = new HashSet();
+			while (i.hasNext())
+				res.add(((JavaAnnotation)i.next()).getType());
+			return new UnitResult(res);
+		}        
       
-      return null;
-    }
+		return null;
+	}
+    
+	/**
+	 * Custom deserialization of this object
+	 */
+	private void readObject(ObjectInputStream in) throws IOException,ClassNotFoundException
+	{
+		// Nothing yet
+	}
+	
+	/**
+	 * Custom serialization of this object
+	 */
+	private void writeObject(ObjectOutputStream out) throws IOException
+	{
+		// Nothing yet
+	}
 }
+
