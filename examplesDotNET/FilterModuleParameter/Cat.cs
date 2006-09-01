66c17aa1368ee67b539566176ee0d8687574271f
Dirk Doornenbal <>
2006-09-01 13:57:48 +0000

    Added FilterModule Parameter example

---
+++ examplesDotNET/FilterModuleParameter/Cat.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace FilterModuleParameter
+{
+	/// <summary>
+	/// Summary description for Cat.
+	/// </summary>
+	public class Cat
+	{
+		public Cat()
+		{
+		}
+
+		public void feed()
+		{
+			Console.Out.WriteLine("eating catfood");
+		}
+	}
+}
---
+++ examplesDotNET/FilterModuleParameter/Iguanidae.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace FilterModuleParameter
+{
+	/// <summary>
+	/// Summary description for Iguanidae.
+	/// </summary>
+	public class Iguanidae
+	{
+		public Iguanidae()
+		{
+		}
+
+		public void feed()
+		{
+			Console.Out.WriteLine("eating letuce");
+		}
+	}
+}
