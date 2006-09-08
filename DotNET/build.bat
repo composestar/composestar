1a5d6ade1f208bd82822dde8fab27043caaf9811
Rolf Huisman <>
2006-09-08 12:39:33 +0000

    Modified the examples so the references are location independent.
    In human language: Making sure that it works when you checkout anywhere.

--- DotNET/build.bat
+++ DotNET/build.bat
@@ -1,4 +1,4 @@
 @echo off
-cd Core&&cd compiletime&&make clean&&make&&cd ..&&cd runtime&&make clean&&make&&cd ..&&cd..&&cd DotNet&&cd compiletime&&make&&cd ..&&cd runtime&&make clean&&make&&cd ..&&cd plugin&&cd ComposestarVSAddin&&make&&cd ..&&cd ..&&cd ..
+cd Core&&cd compiletime&&make clean&&make&&cd ..&&cd runtime&&make clean&&make&&cd ..&&cd..&&cd DotNet&&cd compiletime&&make&&cd ..&&cd runtime&&make clean&&make&&cd ..&&cd plugin&&cd VSAddin&&make&&cd ..&&cd ..&&cd ..
 
 
