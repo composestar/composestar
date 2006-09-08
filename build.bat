b1b1e9a67672a9d305e1a4a30ba89ed0c063e22e
Rolf Huisman <>
2006-09-08 07:50:43 +0000

    Fixing dummer compile

--- build.bat
+++ build.bat
@@ -1,31 +1,4 @@
-cd Core
-	Cd compiletime
-		make clean
-		make
-	cd ..
-
-	cd runtime
-		make clean
-		make
-	cd ..
-cd..
-
-cd DotNet
-	cd compiletime
-rem		make clean
-		make
-	cd ..
-	
-	cd runtime
-		make clean
-		make
-	cd ..
-
-        cd plugin
-                cd ComposestarVSAddin
-                        make
-                cd ..
-        cd ..
-cd ..
+@echo off
+cd Core&&cd compiletime&&make clean&&make&&cd ..&&cd runtime&&make clean&&make&&cd ..&&cd..&&cd DotNet&&cd compiletime&&make&&cd ..&&cd runtime&&make clean&&make&&cd ..&&cd plugin&&cd ComposestarVSAddin&&make&&cd ..&&cd ..&&cd ..
 
 
