02dd206e746dd982331d695d215870f0d6edd777
Marcus Klimstra <>
2006-09-13 11:13:07 +0000

    Simplified INCRE by replacing reflective call to CTCommonModule.run to interface call

--- Core/Compiletime/src/Composestar/Core/INCRE/Module.java
+++ Core/Compiletime/src/Composestar/Core/INCRE/Module.java
@@ -1,14 +1,15 @@
 package Composestar.Core.INCRE;
 
-import Composestar.Utils.*;
-import Composestar.Core.Exception.*;
-import Composestar.Core.Master.CommonResources;
-
-import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.Map;
+
+import Composestar.Core.Exception.ModuleException;
+import Composestar.Core.Master.CTCommonModule;
+import Composestar.Core.Master.CommonResources;
+import Composestar.Utils.Debug;
 
 public class Module 
 {
@@ -18,26 +19,22 @@ public class Module
 	private boolean enabled = false;
 	private boolean incremental = false;
 	private String summary = "";
-	   
-   /**
-    * A map containing dependencies for the module. 
-    */
-    private LinkedHashMap deps = new LinkedHashMap();
-   
-    /**
-     * A map containing objects to be compared by MyComparator. 
-     */
-    private HashMap comparableObjects = new HashMap();
-    private static final Object[] EmptyObjectArray = {};
 
-    /**
-     * @roseuid 420A1C42000F
-     */
-     public Module(String name)
-     {
-         this.name = name;
-     }
-   
+	/**
+	 * A map containing dependencies for the module. 
+	 */
+	private Map deps = new LinkedHashMap();
+
+	/**
+	 * A map containing objects to be compared by MyComparator. 
+	 */
+	private Map comparableObjects = new HashMap();
+
+	public Module(String name)
+	{
+		this.name = name;
+	}
+
 	public void setFullType(String ftype)
 	{
 		this.fulltype = ftype;
@@ -63,27 +60,27 @@ public class Module
 		String id = d.getName();
 		this.deps.put(id,d);
 	}
-   
+
 	public void addComparableObject(String key, Object obj){
 		ArrayList list;
 		if(this.comparableObjects.containsKey(key))
 			list = (ArrayList)comparableObjects.get(key);
 		else list = new ArrayList();
-		
+
 		list.add(obj);
 		this.comparableObjects.put(key,list);
 	}
-	
+
 	public void removeComparableObject(String key, Object obj){
 		ArrayList list;
 		if(this.comparableObjects.containsKey(key))
 			list = (ArrayList)comparableObjects.get(key);
 		else list = new ArrayList();
-		
+
 		list.remove(obj);
 		this.comparableObjects.put(key,list);
 	}
-	
+
 	public void addComparableObjects(HashMap map){
 		Iterator keys = map.keySet().iterator();
 		while(keys.hasNext()){
@@ -96,7 +93,7 @@ public class Module
 			}
 		}
 	}
-	
+
 	public void removeComparableObjects(HashMap map){
 		Iterator keys = map.keySet().iterator();
 		while(keys.hasNext()){
@@ -109,15 +106,15 @@ public class Module
 			}
 		}
 	}
-	
+
 	public boolean hasComparableObjects(String key){
 		return comparableObjects.containsKey(key);	
 	}
-	
+
 	public ArrayList getComparableObjects(String key){
 		return (ArrayList)comparableObjects.get(key);	
 	}
-	
+
 	public Iterator getDeps() 
 	{
 		return deps.values().iterator();
@@ -149,39 +146,42 @@ public class Module
 	 */
 	public void execute(CommonResources resources) throws ModuleException
 	{
-		if(this.enabled)
+		if (this.enabled)
 		{
 			// module is enabled for the phase so continue
-			if(this.summary.length() != 0)
+			if (this.summary.length() != 0)
 				Debug.out(Debug.MODE_CRUCIAL, "Master",this.summary);
-		
+
 			try 
 			{
-				Class myclass = Class.forName(this.fulltype);
-				Class[] methodparams = {resources.getClass()};
-				Method runmethod = myclass.getMethod("run",methodparams);
-				Object[] myparams = {resources};
-				if(myparams == null){
-					myparams = EmptyObjectArray;
-				}
+				Class moduleClass = Class.forName(this.fulltype);
+			/*
+				Class[] paramTypes = { resources.getClass() };
+				Method runmethod = moduleClass.getMethod("run", paramTypes);
+				Object[] params = {resources};
 				INCRE incre = INCRE.instance();
-				INCRETimer module = incre.getReporter().openProcess(this.name,this.name,INCRETimer.TYPE_ALL);
-				runmethod.invoke(myclass.newInstance(),myparams);
-				module.stop();
+				INCRETimer timer = incre.getReporter().openProcess(this.name,this.name,INCRETimer.TYPE_ALL);
+				runmethod.invoke(moduleClass.newInstance(),params);
+				timer.stop();
+			*/
+				CTCommonModule module = (CTCommonModule)moduleClass.newInstance();
+				INCRETimer timer = INCRE.instance().getReporter().openProcess(this.name,this.name,INCRETimer.TYPE_ALL);
+				module.run(resources);
+				timer.stop();
 			}
-			catch(StackOverflowError ex1){
+			catch (StackOverflowError ex1){
 				ex1.printStackTrace();
 				throw new ModuleException("I need more stack!", "INCRE running " + this.name);
 			}
-			catch(OutOfMemoryError ex2){
+			catch (OutOfMemoryError ex2){
 				ex2.printStackTrace();
 				throw new ModuleException("I am using too much memory!", "INCRE running " + this.name);
 			}
-			catch(ClassNotFoundException ex3){
+			catch (ClassNotFoundException ex3){
 				ex3.printStackTrace();
 				throw new ModuleException("Cannot find class "+this.fulltype, "INCRE running " + this.name);
 			}
-			catch(InvocationTargetException ex4){
+		/*	catch (InvocationTargetException ex4){
 				ex4.printStackTrace();
 				if (ex4.getCause() instanceof ModuleException)
 					throw (ModuleException)ex4.getCause();
@@ -189,20 +189,20 @@ public class Module
 					throw (ModuleException)ex4.getTargetException();
 				else
 					throw new ModuleException(ex4.getMessage(), "INCRE running " + this.name);
-			}
-			catch(IllegalAccessException ex5){
+			}*/
+			catch (IllegalAccessException ex5){
 				ex5.printStackTrace();
 				throw new ModuleException(ex5.getMessage() + " Caused by:" + ex5.getCause().getMessage(), "INCRE running " + this.name);
 			}
-			catch(NoSuchMethodException ex6){
+		/*	catch (NoSuchMethodException ex6){
+				ex6.printStackTrace();
+				throw new ModuleException(ex6.getMessage(), "INCRE running " + this.name);
+			}*/
+			catch (InstantiationException ex6){
 				ex6.printStackTrace();
 				throw new ModuleException(ex6.getMessage(), "INCRE running " + this.name);
 			}
-			catch(InstantiationException ex6){
-				ex6.printStackTrace();
-				throw new ModuleException(ex6.getMessage(), "INCRE running " + this.name);
-			}
-			catch(Exception e){
+			catch (Exception e){
 				e.printStackTrace();
 				Debug.out(Debug.MODE_DEBUG, "Master",e.getCause().toString());
 				throw new ModuleException(e.getCause().toString(),"INCRE running " + this.name);
