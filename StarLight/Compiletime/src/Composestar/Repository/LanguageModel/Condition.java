97a6d494197663d239c64b15507946eecaa7fd94
Arjan de Roo <>
2006-09-15 14:14:18 +0000

    Added Condition, Internal and External

---
+++ StarLight/Compiletime/src/Composestar/Repository/LanguageModel/Condition.java
@@ -0,0 +1,58 @@
+package Composestar.Repository.LanguageModel;
+
+/**
+ * Summary description for Condition.
+ */
+public class Condition
+{
+	public Condition()
+	{
+		//
+		// TODO: Add Constructor Logic here
+		//
+	}
+
+	private String _name;
+
+	/** @property */
+	public String get_Name()
+	{
+		return _name;
+	}
+
+	/** @property */
+	public void set_Name(String value)
+	{
+		_name = value;
+	}
+
+
+	private Reference _reference;
+
+	/** @property */
+	public Reference get_Reference()
+	{
+		return _reference;
+	}
+
+	/** @property */
+	public void set_Reference(Reference value)
+	{
+		_reference = value;
+	}
+
+
+	private int _parentTypeId;
+
+	/** @property */
+	public void set_ParentTypeId(int value)
+	{
+		_parentTypeId = value;
+	}
+
+	/** @property */
+	public int get_ParentTypeId()
+	{
+		return _parentTypeId;
+	}
+}
