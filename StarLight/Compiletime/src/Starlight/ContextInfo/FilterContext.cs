95931c80d0c3077f652ea7a45c988b3b7591a9bd
Michiel van Oudheusden <mivano@users.noreply.github.com>
2006-09-19 11:53:46 +0000

    Added some IL code to the weaver.

--- StarLight/Compiletime/src/Starlight/ContextInfo/FilterContext.cs
+++ StarLight/Compiletime/src/Starlight/ContextInfo/FilterContext.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
-using System.Runtime.CompilerServices;  
- 
+using System.Runtime.CompilerServices;
+
 namespace Composestar.StarLight.ContextInfo
 {
     /// <summary>
@@ -15,47 +15,61 @@ namespace Composestar.StarLight.ContextInfo
 
         // TODO Not complete yet
 
-        private Dictionary<int, InnerFilterContext> _innercalls;
+        private static Dictionary<int, InnerFilterContext> _innercalls;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:FilterContext"/> class.
         /// </summary>
-        public FilterContext()
+        static FilterContext()
         {
             _innercalls = new Dictionary<int, InnerFilterContext>();
         }
 
+        /// <summary>
+        /// Determines whether [is inner call] [the specified current instance].
+        /// </summary>
+        /// <param name="currentInstance">The current instance.</param>
+        /// <param name="methodSignature">The method signature.</param>
+        /// <returns>
+        /// 	<c>true</c> if [is inner call] [the specified current instance]; otherwise, <c>false</c>.
+        /// </returns>
         [MethodImpl(MethodImplOptions.Synchronized)]
-        public bool IsInnerCall(object currentInstance, string methodName)
+        public static bool IsInnerCall(object currentInstance, string methodSignature)
         {
-           
-                InnerFilterContext ifc = GetInnerFilterContext();
-                if (ifc == null)
-                    return false;
-                else
-                    return true;
-         
+
+            InnerFilterContext ifc = GetInnerFilterContext();
+            if (ifc == null)
+                return false;
+            else
+                return (ifc.Instance.Equals(currentInstance) & ifc.MethodSignature.Equals(methodSignature));
+
+        }
+
+        public static void SetInnerCall(object currentInstance, string methodSignature)
+        {
+            InnerFilterContext ifc =new InnerFilterContext(currentInstance, methodSignature);
+            _innercalls.Add(GetThreadId(), ifc); 
         }
 
         /// <summary>
         /// Resets the inner call.
         /// </summary>
         [MethodImpl(MethodImplOptions.Synchronized)]
-        public void ResetInnerCall()
+        public static void ResetInnerCall()
         {
-            _innercalls.Remove(GetThreadId()); 
+            _innercalls.Remove(GetThreadId());
         }
-        
+                
         /// <summary>
         /// Gets the inner filter context.
         /// </summary>
         /// <returns></returns>
-        private InnerFilterContext GetInnerFilterContext()
+        private static InnerFilterContext GetInnerFilterContext()
         {
             int threadId = GetThreadId();
- 
+
             InnerFilterContext ifc;
-            if (_innercalls.TryGetValue(threadId, out ifc) )
+            if (_innercalls.TryGetValue(threadId, out ifc))
                 return ifc;
             else
                 return null;
@@ -65,7 +79,7 @@ namespace Composestar.StarLight.ContextInfo
         /// Gets the thread id.
         /// </summary>
         /// <returns></returns>
-        private int GetThreadId()
+        private static int GetThreadId()
         {
             return Thread.CurrentThread.ManagedThreadId;
         }
@@ -73,18 +87,42 @@ namespace Composestar.StarLight.ContextInfo
         private sealed class InnerFilterContext
         {
 
-            private string _methodName;
+            /// <summary>
+            /// Initializes a new instance of the <see cref="T:InnerFilterContext"/> class.
+            /// </summary>
+            /// <param name="instance">The instance.</param>
+            /// <param name="methodSignature">The method signature.</param>
+            public InnerFilterContext(object instance, string methodSignature)
+            {
+                _instance = instance;
+                _methodSignature = methodSignature;
+            }
+
+            private string _methodSignature;
 
             /// <summary>
             /// Gets or sets the name of the method.
             /// </summary>
             /// <value>The name of the method.</value>
-            public string MethodName
+            public string MethodSignature
             {
-                get { return _methodName; }
-                set { _methodName = value; }
+                get { return _methodSignature; }
+                set { _methodSignature = value; }
             }
-	
+
+            private object _instance;
+
+            /// <summary>
+            /// Gets or sets the instance.
+            /// </summary>
+            /// <value>The instance.</value>
+            public object Instance
+            {
+                get { return _instance; }
+                set { _instance = value; }
+            }
+
+
         }
     }
 }
