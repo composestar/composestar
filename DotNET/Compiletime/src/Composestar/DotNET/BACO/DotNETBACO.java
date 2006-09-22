0529b2bff50493c29a2a7d607434837e811a059c
Marcus Klimstra <>
2006-09-22 11:47:53 +0000

    Replaced use of deprecated method with FileUtils method.

--- DotNET/Compiletime/src/Composestar/DotNET/BACO/DotNETBACO.java
+++ DotNET/Compiletime/src/Composestar/DotNET/BACO/DotNETBACO.java
@@ -1,37 +1,34 @@
 package Composestar.DotNET.BACO;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 
 import Composestar.Core.BACO.BACO;
 import Composestar.Core.Master.Config.Dependency;
 import Composestar.Core.RepositoryImplementation.DataStore;
+import Composestar.Utils.FileUtils;
 
-public class DotNETBACO extends BACO{
-
-	protected boolean checkNeededDependency(Dependency dependency){
+public class DotNETBACO extends BACO
+{
+	protected boolean checkNeededDependency(Dependency dependency)
+	{
+		// FIXME: assumption about .NET installation
 		return !(dependency.getFileName().indexOf("Microsoft.NET/Framework/") > 0);
 	}
 	
-	protected void copyBuildAssemblies(HashSet filesToCopy){
-        Iterator it = ((ArrayList)DataStore.instance().getObjectByID("BuiltLibs")).iterator();
-        while(it.hasNext())
+	protected void copyBuildAssemblies(HashSet filesToCopy)
+	{
+		List builtLibs = (List)DataStore.instance().getObjectByID("BuiltLibs");
+        Iterator it = builtLibs.iterator();
+        while (it.hasNext())
         {
-        	String file = this.processString((String)it.next());
-        	file = removeExtention(file) + ".pdb";
-        	//System.err.println("COPY: "+composestarpath+"binaries\\\t"+it.next());
-        	filesToCopy.add(file);
+        	String dll = FileUtils.unquote((String)it.next());
+        	String pdb = FileUtils.removeExtension(dll) + ".pdb";
+
+        	filesToCopy.add(pdb);
         }
+        
         super.copyBuildAssemblies(filesToCopy);
-	}
-	
-	protected String removeExtention(String fileName){
-		if(fileName == null)
-			return null;
-		int index = fileName.lastIndexOf('.');
-		if(index < 0)
-			return fileName;
-		return fileName.substring(0,index);
 	}
 }
