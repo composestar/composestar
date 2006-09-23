49857adfe9b6dc27607178fcd982bd57092d1514
Dirk Doornenbal <>
2006-09-23 11:31:30 +0000

    Changed FilterElementAST to ContextRepositoryEntity. So also the mails on the mailing list.
         .'`~~~~~~~~~~~`'.
         (  .'11 12 1'.  )
         |  :10 \    2:  |
         |  :9   @-> 3:  |
         |  :8       4;  |
         '. '..7 6 5..' .'
          ~-------------~  
    
    Fact:
    Filterelement did extend RepositoryEntity since:
    Fri Jul 16 00:39:57 2004 UTC (2 years, 2 months ago) by fladder

--- Core/Compiletime/src/Composestar/Core/CpsProgramRepository/CpsConcern/Filtermodules/ParameterizedMessageSelectorAST.java
+++ Core/Compiletime/src/Composestar/Core/CpsProgramRepository/CpsConcern/Filtermodules/ParameterizedMessageSelectorAST.java
@@ -2,9 +2,11 @@ package Composestar.Core.CpsProgramRepository.CpsConcern.Filtermodules;
 
 public class ParameterizedMessageSelectorAST extends MessageSelectorAST {
 	public String parameter;
+	public boolean list;
 	
 	public ParameterizedMessageSelectorAST() {
 		super();
+		list = false; //by default
 	}
 
 	public String getParameter() {
@@ -13,5 +15,13 @@ public class ParameterizedMessageSelectorAST extends MessageSelectorAST {
 
 	public void setParameter(String parameter) {
 		this.parameter = parameter;
+	}
+
+	public boolean isList() {
+		return list;
+	}
+
+	public void setList(boolean isList) {
+		this.list = isList;
 	}
 }
