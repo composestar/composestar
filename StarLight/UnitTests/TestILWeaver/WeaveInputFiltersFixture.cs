babc6b4edc0f2a1d584c026d5715d0525eccb575
Michiel van Oudheusden <mivano@users.noreply.github.com>
2006-09-25 14:33:17 +0000

    Added IL check for error filter

--- StarLight/UnitTests/TestILWeaver/WeaveInputFiltersFixture.cs
+++ StarLight/UnitTests/TestILWeaver/WeaveInputFiltersFixture.cs
@@ -13,42 +13,44 @@ using Composestar.Repository.LanguageModel;
 using Composestar.Repository.LanguageModel.Inlining;
 using Composestar.StarLight.CoreServices;
 using Composestar.StarLight.ILWeaver;
+
 using TestILWeaver.DIConfiguration;
 using TestILWeaver.Mocks;
+using Testing.CecilILReader;
 
 namespace TestILWeaver
 {
     [TestClass]
     public class WeaveInputFiltersFixture : ILWeaverFixtureBase
     {
- 
+
         [TestMethod]
         [DeploymentItem(TestInputImage)]
         public void WeavingErrorActionAddsExceptionFilter()
         {
             string OutputFileName = "WeavingErrorActionAddsExceptionFilter.exe";
-            
+
             // set up model 
             LanguageModelAccessorMock model = new LanguageModelAccessorMock();
-           
+
             // Create inputfilters
             Block block = new Block();
             Block block2 = new Block();
-                    
+
             ContextInstruction ci = new ContextInstruction(ContextInstruction.CHECK_INNER_CALL, 1000, block2);
             ContextInstruction ci2 = new ContextInstruction(ContextInstruction.RESET_INNER_CALL, 1000, null);
             ContextInstruction ci3 = new ContextInstruction(ContextInstruction.SET_INNER_CALL, 1000, null);
-            
+
             FilterAction errorAction = new FilterAction("ErrorAction", "", "");
-        
+
             block2.addInstruction(errorAction);
- 
+
             block.addInstruction(ci);
             block.addInstruction(ci2);
             block.addInstruction(ci3);
-          
+
             model.AddInputFilter("TestTarget.Program", "System.Void TestMethod(System.Int32)", block);
-                     
+
             // create configuration
             CecilWeaverConfiguration configuration = CecilWeaverConfiguration.CreateDefaultConfiguration(CreateFullPath("TestTarget.exe"), CreateFullPath(OutputFileName));
 
@@ -56,16 +58,17 @@ namespace TestILWeaver
             CecilILWeaver weaver = DIHelper.CreateObject<CecilILWeaver>(CreateTestContainer(model, configuration));
             weaver.DoWeave();
 
+            ILReader il = new ILReader();
+            il.OpenAssembly(CreateFullPath(OutputFileName));
+            List<ILInstruction> ils = il.GetILInstructions("TestTarget.Program", "TestMethod");
 
-            // test wether Internal is created
-            Assembly asm = Assembly.LoadFile(CreateFullPath(OutputFileName));
-            Type programType = asm.GetType("TestTarget.Program");
-         //   FieldInfo internalStringField = programType.GetField("InternalString", BindingFlags.NonPublic | BindingFlags.Instance);
+            // Expecting the following IL code
+            List<ILInstruction> shouldContainThrow = new List<ILInstruction>();
+            shouldContainThrow.Add(new ILInstruction(0, "newobj", "System.Void System.Exception::.ctor()"));
+            shouldContainThrow.Add(new ILInstruction(0, "throw", ""));
 
-            // assert on requirements
-            Assert.IsNotNull(programType);
-          //  Assert.AreEqual(typeof(string), internalStringField.FieldType);
-            Assert.Inconclusive(); 
+            Assert.IsTrue(il.ContainsILInstructions(ils, shouldContainThrow), "Generated IL code did not contain the throw exception code");
+
         }
 
         [TestMethod]
@@ -76,25 +79,25 @@ namespace TestILWeaver
 
             // set up model 
             LanguageModelAccessorMock model = new LanguageModelAccessorMock();
-           
+
             // Create inputfilters
             Block block = new Block();
             Block block2 = new Block();
-                    
+
             ContextInstruction ci = new ContextInstruction(ContextInstruction.CHECK_INNER_CALL, 1000, block2);
             ContextInstruction ci2 = new ContextInstruction(ContextInstruction.RESET_INNER_CALL, 1000, null);
             ContextInstruction ci3 = new ContextInstruction(ContextInstruction.SET_INNER_CALL, 1000, null);
-            
+
             FilterAction errorAction = new FilterAction("BeforeAction", "", "");
-        
+
             block2.addInstruction(errorAction);
- 
+
             block.addInstruction(ci);
             block.addInstruction(ci2);
             block.addInstruction(ci3);
-          
+
             model.AddInputFilter("TestTarget.Program", "System.Void TestMethod(System.Int32)", block);
-                     
+
             // create configuration
             CecilWeaverConfiguration configuration = CecilWeaverConfiguration.CreateDefaultConfiguration(CreateFullPath("TestTarget.exe"), CreateFullPath(OutputFileName));
 
@@ -102,16 +105,7 @@ namespace TestILWeaver
             CecilILWeaver weaver = DIHelper.CreateObject<CecilILWeaver>(CreateTestContainer(model, configuration));
             weaver.DoWeave();
 
-
-            // test wether Internal is created
-            Assembly asm = Assembly.LoadFile(CreateFullPath(OutputFileName));
-            Type programType = asm.GetType("TestTarget.Program");
-         //   FieldInfo internalStringField = programType.GetField("InternalString", BindingFlags.NonPublic | BindingFlags.Instance);
-
-            // assert on requirements
-            Assert.IsNotNull(programType);
-          //  Assert.AreEqual(typeof(string), internalStringField.FieldType);
-            Assert.Inconclusive(); 
+            Assert.Inconclusive();
         }
     }
 }
