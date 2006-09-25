39af94a8623c645e0275432b3fcf40326d3f0dd4
Marcus Klimstra <>
2006-09-25 11:45:08 +0000

    DotNETSignatureTransformer: will now throw an exception on error instead of justing printing a stacktrace.

--- DotNET/Compiletime/src/Composestar/DotNET/TYM/SignatureTransformer/DotNETSignatureTransformer.java
+++ DotNET/Compiletime/src/Composestar/DotNET/TYM/SignatureTransformer/DotNETSignatureTransformer.java
@@ -1,74 +1,64 @@
-//Source file: C:\\local\\staijen\\composestar\\src\\Composestar\\CTAdaption\\TYM\\AssemblyTransformer\\AssemblyTransformer.java
-
 package Composestar.DotNET.TYM.SignatureTransformer;
 
-import Composestar.Core.TYM.SignatureTransformer.*;
+import java.util.Iterator;
+import java.util.List;
+
+import Composestar.Core.CpsProgramRepository.Concern;
+import Composestar.Core.Exception.ModuleException;
 import Composestar.Core.Master.CommonResources;
 import Composestar.Core.Master.Config.Configuration;
 import Composestar.Core.RepositoryImplementation.DataStore;
+import Composestar.Core.TYM.SignatureTransformer.SignatureTransformer;
 import Composestar.Utils.Debug;
-import Composestar.Core.CpsProgramRepository.Concern;
-import Composestar.Core.Exception.ModuleException;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+public class DotNETSignatureTransformer implements SignatureTransformer
+{    
+	public DotNETSignatureTransformer()
+	{
+	}
 
-public class DotNETSignatureTransformer implements SignatureTransformer {
-    
-    /**
-     * @roseuid 40F632500138
-     */
-    public DotNETSignatureTransformer() {
-     
-    }
-    
-    /**
-     * @param resources
-     * @throws Composestar.core.Exception.ModuleException
-     * @roseuid 40F632500157
-     */
-    public void run(CommonResources resources) throws ModuleException {
-     	
-		boolean signaturesmodified = ((Boolean)(resources.getResource("signaturesmodified"))).booleanValue();
-		if( signaturesmodified )
+	public void run(CommonResources resources) throws ModuleException
+	{
+		// only run ASTRA when SIGN found ADDED or REMOVED methods
+		boolean sigmod = ((Boolean)(resources.getResource("signaturesmodified"))).booleanValue();
+		if (sigmod)
 		{
-			// only run ASTRA when SIGN found ADDED or REMOVED methods
-			DataStore ds = DataStore.instance();
 			ILCodeParser codeParser = new ILCodeParser();
+
+			DataStore ds = DataStore.instance();
 			Iterator iterator = ds.getAllInstancesOf(Concern.class);
-			while(iterator.hasNext())
+			while (iterator.hasNext())
 			{
-				Concern concern = (Concern) iterator.next();
+				Concern concern = (Concern)iterator.next();
 				codeParser.addConcern(concern);    
 			}
-     	
+
 			try
 			{
-				ArrayList dummies = Configuration.instance().getProjects().getCompiledDummies();
+				List dummies = Configuration.instance().getProjects().getCompiledDummies();
 				Iterator dumIt = dummies.iterator();
-				while(dumIt.hasNext()) 
+				while (dumIt.hasNext()) 
 				{
 					String name = (String)dumIt.next();
 					codeParser.setAssemblyName(name);
 					codeParser.run();
-		        }
-				
-				/*String assemblies = Configuration.instance().getModuleSettings().getModule("ILICIT").getProperty("assemblies");
+				}
+
+			/*	TODO: can this be removed?
+				String assemblies = Configuration.instance().getModuleSettings().getModule("ILICIT").getProperty("assemblies");
 				String[] assemblyArray = assemblies.split(",");
 				for( int i = 0; i < assemblyArray.length; i++ )
 				{
 					codeParser.setAssemblyName(assemblyArray[i]);
 					codeParser.run();
-				}*/
+				} */
 			}
-			catch(ModifierException me)
+			catch (ModifierException e)
 			{
-				me.printStackTrace();
+				throw new ModuleException(e.getMessage(), "ASTRA");
 			}
 		}
 		else 
-		{
-			Debug.out (Debug.MODE_INFORMATION, "ASTRA", "No need to transform assemblies");
-		}
-    }
+			Debug.out(Debug.MODE_INFORMATION, "ASTRA", "No need to transform assemblies");
+	}
 }
