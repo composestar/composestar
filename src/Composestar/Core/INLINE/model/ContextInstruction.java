4afa092c3033151978877c65ba086a8ebc59ad0f
Arjan de Roo <>
2006-09-27 07:57:31 +0000

    some changes to the inliner

--- src/Composestar/Core/INLINE/model/ContextInstruction.java
+++ src/Composestar/Core/INLINE/model/ContextInstruction.java
@@ -9,21 +9,23 @@ import Composestar.Core.LAMA.MethodInfo;
 public class ContextInstruction extends Instruction{
     private int type;
     private MethodInfo method;
-    private Block innerBlock;
+    private Instruction instruction;
 
     public final static int REMOVED = 0;
     public final static int SET_INNER_CALL = 10;
     public final static int CHECK_INNER_CALL = 11;
     public final static int RESET_INNER_CALL = 12;
+    public final static int STORE_ACTION = 20;
+    public final static int EXECUTE_STORED_ACTIONS = 21;
 
     public ContextInstruction( int type, MethodInfo method ){
         this.type = type;
         this.method = method;
     }
     
-    public ContextInstruction( int type, MethodInfo method, Block innerBlock ){
+    public ContextInstruction( int type, MethodInfo method, Instruction instruction ){
         this( type, method );
-        this.innerBlock = innerBlock;
+        this.instruction = instruction;
     }
 
     /**
@@ -48,10 +50,10 @@ public class ContextInstruction extends Instruction{
     }
 
     /**
-     * @return the innerBlock
+     * @return the instruction
      */
-    public Block getInnerBlock(){
-        return innerBlock;
+    public Instruction getInstruction(){
+        return instruction;
     }
 
     /**
