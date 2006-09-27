4f6293052341d4f4854254c7f47ae16bc076f9ab
Marcus Klimstra <>
2006-09-27 14:43:24 +0000

    CommonResources: added a couple of FIXME's. (Seems like this class isn't used very often?)

--- src/Composestar/Core/Master/CommonResources.java
+++ src/Composestar/Core/Master/CommonResources.java
@@ -1,5 +1,3 @@
-//Source file: C:\\local\\staijen\\composestar\\src\\Composestar\\core\\Master\\CommonResources.java
-
 /*
  * This file is part of Composestar project [http://composestar.sf.net].
  * Copyright (C) 2003 University of Twente.
@@ -12,69 +10,71 @@
 
 package Composestar.Core.Master;
 
-import Composestar.Core.RepositoryImplementation.DataMap;
-import java.util.Properties;
 import java.io.Serializable;
+import java.util.Properties;
+
+import Composestar.Core.RepositoryImplementation.DataMap;
+import Composestar.Utils.Debug;
 
 /**
- * This class holds the shared resources between the modules e.g the repository 
- * object.
- * @todo Add hashmap for storing non-global stuff.
+ * This class holds the shared resources between the modules e.g the repository object.
+ * TODO: Add hashmap for storing non-global stuff.
  */
-public class CommonResources implements Serializable{
-    
-    /**
-	 * 
-	 */
+public class CommonResources implements Serializable
+{
 	private static final long serialVersionUID = 2652039710117430543L;
 
+//	/**
+//	 * The main project configuration file. This resource is initialized by Master
+//	 */
+//	public Properties ProjectConfiguration;
+	
 	/**
-     * The main project configuration file. This resource is initialized by Master
-     */
-    //public Properties ProjectConfiguration;
-    
-    /**
-     * Information about the Custom Filters that are used in this project.
-     */
-    public Properties CustomFilters;
-    
-    /**
-     * HashMap holding all private resources.
-     */
-    private DataMap ResourceMap;
-    
-    /**
-     * Default ctor.
-     * @roseuid 401B9C5201F7
-     */
-    public CommonResources() {
-        ResourceMap = new DataMap();     
-    }
-    
-    /**
-     * Add a resource with a key.
-     * 
-     * @param key An identifier for this resource
-     * @param object The object to store for this key.
-     * @roseuid 401D4BEF01E0
-     */
-    public void addResource(String key, Object object) {
-        ResourceMap.put( key, object );     
-    }
-    
-    /**
-     * Fetch a resource with a key.
-     * 
-     * @param key String key for the object you want to retrieve.
-     * @return An object pointer if an object with the specified key was found or null 
-     * if the key is invalid.
-     * @roseuid 401D4C0F0196
-     */
-    public Object getResource(String key) {
-        if( !ResourceMap.containsKey( key ) ) { // if not in map
-            return null;
-        }
+	 * Information about the Custom Filters that are used in this project.
+	 * FIXME: create a getter?
+	 */
+	public Properties CustomFilters;
 
-        return ResourceMap.get( key );     
-    }
+	/**
+	 * Map holding all private resources.
+	 * FIXME: this field is private, so we can use a HashMap?
+	 */
+	private DataMap resources;
+
+	/**
+	 * Default constructor.
+	 */
+	public CommonResources()
+	{
+		resources = new DataMap();
+	}
+
+	/**
+	 * Add a resource with a key.
+	 * 
+	 * @param key An identifier for this resource.
+	 * @param object The object to store for this key.
+	 */
+	public void addResource(String key, Object object)
+	{
+	//	Debug.out(Debug.MODE_DEBUG,"RES","Added resource: " + key + " -> " + object.getClass());
+		resources.put(key, object);
+	}
+
+	/**
+	 * Fetch a resource with a key.
+	 * 
+	 * @param key key for the object you want to retrieve.
+	 * @return An object pointer if an object with the specified key was found 
+	 * or null if the key is invalid.
+	 */
+	public Object getResource(String key)
+	{
+	//	Debug.out(Debug.MODE_DEBUG,"RES","Requested resource: " + key);
+
+		if (!resources.containsKey(key))
+			return null;
+
+		return resources.get(key);
+	}
 }
