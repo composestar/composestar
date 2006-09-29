fdd23a15ec3dbe0aee83073d123379874012dddd
Arjan de Roo <>
2006-09-29 12:47:41 +0000

    some changes

--- src/Composestar/Core/INLINE/model/ContextInstruction.java
+++ src/Composestar/Core/INLINE/model/ContextInstruction.java
@@ -16,6 +16,7 @@ public class ContextInstruction extends Instruction{
     public final static int RESET_INNER_CALL = 12;
     public final static int CREATE_ACTION_STORE = 20;
     public final static int STORE_ACTION = 21;
+    public final static int RETURN_ACTION = 100;
 
 
     public ContextInstruction( int type ){
