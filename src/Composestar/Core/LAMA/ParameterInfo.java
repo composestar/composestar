72148ca2124adf3524665e6b42ea0a577b1d382d
Marcus Klimstra <>
2006-10-02 13:57:24 +0000

    some changes

--- src/Composestar/Core/LAMA/ParameterInfo.java
+++ src/Composestar/Core/LAMA/ParameterInfo.java
@@ -9,9 +9,9 @@ public abstract class ParameterInfo extends ProgramElement
 	public String ParameterTypeString;
 	private Type ParameterType;
 	public String Name;
-	
+
 	public MethodInfo Parent;
-	
+
 	public ParameterInfo()
 	{
 		UnitRegister.instance().registerLanguageUnit(this);
@@ -25,16 +25,16 @@ public abstract class ParameterInfo extends ProgramElement
 	{
 		return Name;     
 	}
-	
+
 	/**
-     * @param name
-     * @roseuid 402A072800EE
-     */
-    public void setName(String name) {
-        Name = name;     
-    }
-    
-    
+	 * @param name
+	 * @roseuid 402A072800EE
+	 */
+	public void setName(String name) {
+		Name = name;     
+	}
+
+
 	/**
 	 * @return Composestar.Core.LAMA.Type
 	 * @roseuid 401B84CF021D
@@ -48,7 +48,7 @@ public abstract class ParameterInfo extends ProgramElement
 		}
 		return ParameterType;     
 	}
-	
+
 	/**
 	 * @param paramType
 	 * @roseuid 402A0736033D
@@ -57,49 +57,45 @@ public abstract class ParameterInfo extends ProgramElement
 	{
 		ParameterTypeString = paramType;     
 	}
-	
+
 	/**
-     * @return Returns the parent.
-     */
-    public MethodInfo getParent()
-    {
-      return Parent;
-    }
-    
-    /**
-     * @param parent The parent to set.
-     */
-    public void setParent(MethodInfo parent)
-    {
-      Parent = parent;
-    }
-    
-	/** Stuff for LOLA **/
-	
-	/* (non-Javadoc)
-	 * @see Composestar.Core.LAMA.ProgramElement#getUnitName()
+	 * @return Returns the parent.
 	 */
+	public MethodInfo getParent()
+	{
+		return Parent;
+	}
+
+	/**
+	 * @param parent The parent to set.
+	 */
+	public void setParent(MethodInfo parent)
+	{
+		Parent = parent;
+	}
+
+	public String toString()
+	{
+		return ParameterTypeString + " " + Name;
+	}
+
+	// Stuff for LOLA
+
 	public String getUnitName()
 	{
 		return name();
 	}
-	
-	/* (non-Javadoc)
-	 * @see Composestar.Core.LAMA.ProgramElement#getUnitType()
-	 */
+
 	public String getUnitType()
 	{
 		return "Parameter";
 	}
-	
-	/* (non-Javadoc)
-	 * @see Composestar.Core.LAMA.ProgramElement#hasUnitAttribute(java.lang.String)
-	 */
+
 	public boolean hasUnitAttribute(String attribute)
 	{
 		return false;
 	}
-	
+
 	/**
 	 * Custom deserialization of this object
 	 */
@@ -109,7 +105,7 @@ public abstract class ParameterInfo extends ProgramElement
 		ParameterTypeString = in.readUTF();
 		Parent = (MethodInfo)in.readObject();
 	}
-	 
+
 	/**
 	 * Custom serialization of this object
 	 */
