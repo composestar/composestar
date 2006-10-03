12117da0ac3710ae7e208f7351b136ee2e978961
Marcus Klimstra <>
2006-10-03 08:58:40 +0000

    -PathSettings: added getPath(name, default) method. -EMBEX: now outputs to obj/embedded by default.

--- src/Composestar/Core/Master/Config/PathSettings.java
+++ src/Composestar/Core/Master/Config/PathSettings.java
@@ -3,22 +3,27 @@ package Composestar.Core.Master.Config;
 import java.io.Serializable;
 import java.util.Properties;
 
-public class PathSettings implements Serializable{
-
+public class PathSettings implements Serializable
+{
 	private Properties paths;
-	
-	public PathSettings() {
+
+	public PathSettings()
+	{
 		paths = new Properties();
 	}
 
-	public void addPath(String key, String value) {
+	public void addPath(String key, String value)
+	{
 		paths.setProperty(key, value);
 	}
+
+	public String getPath(String key)
+	{
+		return paths.getProperty(key);
+	}
 	
-	public String getPath(String key) {
-		if(paths.containsKey(key))
-			return paths.getProperty(key);
-		return null;
+	public String getPath(String key, String def)
+	{
+		return paths.getProperty(key, def);
 	}
 }
-
