0f0a59d4e568ce922f96c244295ad2f61920912e
Marcus Klimstra <>
2006-10-03 09:49:21 +0000

    -Module: added getProperty(name, default).

--- Core/Compiletime/src/Composestar/Core/INCRE/Config/ConfigManager.java
+++ Core/Compiletime/src/Composestar/Core/INCRE/Config/ConfigManager.java
@@ -1,75 +1,67 @@
 package Composestar.Core.INCRE.Config;
 
-import Composestar.Utils.*;
-import Composestar.Core.INCRE.Module;
-import Composestar.Core.Master.CommonResources;
-
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.xml.sax.XMLReader;
 
+import Composestar.Core.INCRE.Module;
+import Composestar.Core.Master.CommonResources;
+import Composestar.Utils.Debug;
+
 /**
- * Class responsible for parsing configuration files created for the incremental 
- * aspect.
+ * Class responsible for parsing configuration files created for the incremental aspect.
  */
-public class ConfigManager 
+public class ConfigManager
 {
-   private XMLReader xmlreader = null;
-   private INCREXMLParser xmlparser = null; 
-   private CommonResources resources = null;
-   
-   /**
-    * HashMap containing modules. The keys are the names of the modules. The values 
-    * are the module objects.
-    */
-   public LinkedHashMap modules = new LinkedHashMap();
-        
-   /**
-    * @roseuid 420A175602EE
-    */
-   public ConfigManager(CommonResources resources) 
-   {
+	private CommonResources resources;
+	private INCREXMLParser xmlparser;
+	private XMLReader xmlreader;
+
+	/**
+	 * HashMap containing modules. 
+	 * The keys are the names of the modules. 
+	 * The values are the module objects.
+	 */
+	public Map modules = new LinkedHashMap();
+
+	public ConfigManager(CommonResources resources)
+	{
 		this.resources = resources;
 		this.xmlparser = new INCREXMLParser(this);
-   }
-   
-   /**
-    * @roseuid 420A1A120399
-    */
-   public void parseXML(String filename) throws java.io.IOException, org.xml.sax.SAXException 
-   {
-		   xmlreader = org.xml.sax.helpers.XMLReaderFactory.createXMLReader();
-		   xmlreader.setContentHandler(this.xmlparser);
-		   Debug.out(Debug.MODE_DEBUG, "INCRE","Parsing configuration file...");
-		   xmlreader.parse(filename);
-   }
+		this.xmlreader = null;
+	}
+
+	public void parseXML(String filename) throws java.io.IOException, org.xml.sax.SAXException
+	{
+		xmlreader = org.xml.sax.helpers.XMLReaderFactory.createXMLReader();
+		xmlreader.setContentHandler(this.xmlparser);
+		
+		Debug.out(Debug.MODE_DEBUG, "INCRE", "Parsing configuration file...");
+		xmlreader.parse(filename);
+	}
 
 	public XMLReader getXMLReader()
 	{
-		return this.xmlreader;		
-	}
-   
-   /**
-    * @roseuid 420A1A5E00BB
-    */
-   public void addModule(String id,Module m) 
-   {
-	   this.modules.put(id,m);
-   }
-   
-   /**
-    * @roseuid 420A1A64007D
-    */
-	public Module getModuleByID(String id) 
-	{
-		return (Module)modules.get(id);
+		return this.xmlreader;
 	}
 
-	public LinkedHashMap getModules()
+	public void addModule(String id, Module m)
+	{
+		this.modules.put(id, m);
+	}
+
+	public Module getModuleByID(String id)
+	{
+		return (Module) modules.get(id);
+	}
+
+	public Map getModules()
 	{
 		return this.modules;
 	}
-	
+
+	// not used
 	public CommonResources getResources()
 	{
 		return resources;
