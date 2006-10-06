8063a8e4f508d018b09f1482ba195d8b7c474098
Michiel van Oudheusden <mivano@users.noreply.github.com>
2006-10-06 08:06:55 +0000

    The Conditions were using bitwise operators. Now they use branching etc so they generate the correct conditions.

--- StarLight/Source/ILWeaver/Cecil/Visitors/CecilConditionsVisitor.cs
+++ StarLight/Source/ILWeaver/Cecil/Visitors/CecilConditionsVisitor.cs
@@ -16,19 +16,27 @@ namespace Composestar.StarLight.ILWeaver
     /// A visitor used to visit all the condition expressions and build an IL representation of the expressions.
     /// </summary>
     /// <remarks>
-    /// The order of the ConditionExpressions is important. An And expression requires first the two values before 
-    /// it can evaluate the And operator.
+    /// The order of the ConditionExpressions is important. An And expression requires first the two values 
+    /// before it can evaluate the And operator.
     /// </remarks> 
     public class CecilConditionsVisitor : IVisitor
     {
+
+        #region Constant Values
+
+        private const int BranchLabelOffSet = 9000;
+
+        #endregion
 
         #region Private variables
 
         IList<Instruction> _instructions = new List<Instruction>();
         CilWorker _worker;
         MethodDefinition _method;
+        int numberOfBranches = 0;
         AssemblyDefinition _targetAssemblyDefinition;
         ILanguageModelAccessor _languageModelAccessor;  // FIXME we now pass this model through the visitors because we need the repository to get the conditions. Not a good seperation...
+        Dictionary<int, Instruction> _jumpInstructions = new Dictionary<int, Instruction>();
         #endregion
 
         #region Properties
@@ -117,6 +125,26 @@ namespace Composestar.StarLight.ILWeaver
         #region Helper functions
 
         /// <summary>
+        /// Gets the jump label.
+        /// </summary>
+        /// <param name="labelId">The label id.</param>
+        /// <returns></returns>
+        private Instruction GetJumpLabel(int labelId)
+        {
+            if (labelId < 0)
+                return null;
+
+            Instruction jumpNopInstruction;
+            if (!_jumpInstructions.TryGetValue(labelId, out jumpNopInstruction))
+            {
+                jumpNopInstruction = Worker.Create(OpCodes.Nop);
+                _jumpInstructions.Add(labelId, jumpNopInstruction);
+            }
+
+            return jumpNopInstruction;
+        }
+
+        /// <summary>
         /// Creates the method reference.
         /// </summary>
         /// <param name="methodBase">The method base.</param>
@@ -130,17 +158,7 @@ namespace Composestar.StarLight.ILWeaver
 
         #region Condition Visitor Implementation
 
-        /// <summary>
-        /// Visits the and condition.
-        /// </summary>
-        /// <remarks>
-        /// Generate the And instruction.
-        /// </remarks> 
-        /// <param name="and">The and.</param>
-        public void VisitAnd(And and)
-        {
-            Instructions.Add(Worker.Create(OpCodes.And));
-        }
+        #region Variable Value
 
         /// <summary>
         /// Visits the condition literal.
@@ -159,29 +177,28 @@ namespace Composestar.StarLight.ILWeaver
             // Get the type
             Composestar.Repository.LanguageModel.TypeElement te = RepositoryAccess.GetTypeElementById(con.ParentTypeId);
 
-            // TODO Is the con.Reference.Selector unique enough?
             MethodReference method;
-            if ( con.Reference.Target.Equals( Reference.INNER_TARGET ) ||
-                con.Reference.Target.Equals( Reference.SELF_TARGET ) )
+            if (con.Reference.Target.Equals(Reference.INNER_TARGET) ||
+                con.Reference.Target.Equals(Reference.SELF_TARGET))
             {
                 method = CecilUtilities.ResolveMethod(
                     con.Reference.Selector, te.FullName,
-                    te.Assembly, te.FromDLL );
+                    te.Assembly, te.FromDLL);
             }
             else
             {
                 method = CecilUtilities.ResolveMethod(
-                    con.Reference.Selector, 
-                    (con.Reference.NameSpace.Length>0 ? con.Reference.NameSpace + "." : "") + con.Reference.Target,
-                    te.Assembly, te.FromDLL );
+                    con.Reference.Selector,
+                    (con.Reference.NameSpace.Length > 0 ? con.Reference.NameSpace + "." : "") + con.Reference.Target,
+                    te.Assembly, te.FromDLL);
             }
 
             if (method == null)
                 throw new ILWeaverException(String.Format(Properties.Resources.MethodNotFound, con.Reference.Selector, te.FullName, te.Assembly));
 
-            // if ((method.CallingConvention&MethodCallingConvention.ThisCall)== MethodCallingConvention.ThisCall)
-            if (con.Reference.Target.Equals( Reference.INNER_TARGET ) ||
-                con.Reference.Target.Equals( Reference.SELF_TARGET ))
+
+            if (con.Reference.Target.Equals(Reference.INNER_TARGET) ||
+                con.Reference.Target.Equals(Reference.SELF_TARGET))
             {
                 // Load the this pointer
                 Instructions.Add(Worker.Create(OpCodes.Ldarg, Method.This));
@@ -191,6 +208,10 @@ namespace Composestar.StarLight.ILWeaver
             Instructions.Add(Worker.Create(OpCodes.Call, _targetAssemblyDefinition.MainModule.Import(method)));
 
         }
+
+        #endregion
+
+        #region Boolean Values
 
         /// <summary>
         /// Visits the false.
@@ -205,30 +226,6 @@ namespace Composestar.StarLight.ILWeaver
         }
 
         /// <summary>
-        /// Visits the not.
-        /// </summary>
-        /// <remarks>
-        /// Generate the Not instruction.
-        /// </remarks> 
-        /// <param name="not">The not.</param>
-        public void VisitNot(Not not)
-        {
-            Instructions.Add(Worker.Create(OpCodes.Not));
-        }
-
-        /// <summary>
-        /// Visits the or.
-        /// </summary>
-        /// <remarks>
-        /// Generate the Or operator. The two values should be placed on the stack first.
-        /// </remarks> 
-        /// <param name="or">The or.</param>
-        public void VisitOr(Or or)
-        {
-            Instructions.Add(Worker.Create(OpCodes.Or));
-        }
-
-        /// <summary>
         /// Visits the true.
         /// </summary>
         /// <remarks>
@@ -239,6 +236,125 @@ namespace Composestar.StarLight.ILWeaver
         {
             Instructions.Add(Worker.Create(OpCodes.Ldc_I4_1));
         }
+
+        #endregion
+
+        #region Not Operator
+
+        /// <summary>
+        /// Visits the not.
+        /// </summary>
+        /// <remarks>
+        /// Generate the !A instruction.
+        /// <code>
+        /// ldloc A
+        /// ldc.i4.0 
+        /// ceq 
+        /// </code>
+        /// </remarks> 
+        /// <param name="not">The not.</param>
+        public void VisitNot(Not not)
+        {
+            Instructions.Add(Worker.Create(OpCodes.Ldc_I4_0));
+            Instructions.Add(Worker.Create(OpCodes.Ceq));
+        }
+
+        #endregion
+
+        #region Or Operator
+
+        /// <summary>
+        /// Visits the or.
+        /// </summary>
+        /// <remarks>
+        /// Generate the || operator. The two values should be placed on the stack first.
+        /// <code>
+        /// L_0005: ldloc.0 // left
+        /// L_0006: brtrue.s L_000b
+        /// L_0008: ldloc.1 // right
+        /// L_0009: br.s L_000c
+        /// L_000b: ldc.i4.1 
+        /// L_000c: stloc.2 // result
+        /// </code>
+        /// </remarks>         
+        /// <param name="or">The or.</param>
+        public void VisitOrLeft(Or or)
+        {
+            or.BranchId = BranchLabelOffSet + numberOfBranches;
+            numberOfBranches = numberOfBranches + 2;
+
+            Instructions.Add(Worker.Create(OpCodes.Brtrue_S, GetJumpLabel(or.BranchId)));
+        }
+
+        /// <summary>
+        /// Visits the or right.
+        /// </summary>
+        /// <code>
+        /// L_0008: ldloc.1 // right
+        /// L_0009: br.s L_000c
+        /// L_000b: ldc.i4.1 
+        /// L_000c: stloc.2 // result
+        /// </code>
+        /// <param name="or">The or.</param>
+        public void VisitOrRight(Or or)
+        {
+            Instructions.Add(Worker.Create(OpCodes.Br_S, GetJumpLabel(or.BranchId + 1)));
+            Instructions.Add(GetJumpLabel(or.BranchId));
+            Instructions.Add(Worker.Create(OpCodes.Ldc_I4_1));
+            Instructions.Add(GetJumpLabel(or.BranchId + 1));
+        }
+
+        #endregion
+
+        #region And Operator
+
+        /// <summary>
+        /// Visits the left and condition part.
+        /// </summary>
+        /// <remarks>
+        /// Generate the && instruction.
+        /// <code>
+        ///  L_000d: ldloc.0  // Left
+        ///  L_000e: brfalse.s L_0013
+        ///  L_0010: ldloc.1  // Right
+        ///  L_0011: br.s L_0014
+        ///  L_0013: ldc.i4.0 
+        ///  L_0014: stloc.3  // Result 
+        /// </code>
+        /// </remarks> 
+        /// <param name="and">The and.</param>
+        public void VisitAndLeft(And and)
+        {
+            and.BranchId = BranchLabelOffSet + numberOfBranches;
+            numberOfBranches = numberOfBranches + 2;
+            Instructions.Add(Worker.Create(OpCodes.Brfalse_S, GetJumpLabel(and.BranchId)));
+        }
+
+        /// <summary>
+        /// Visits the right and condition part.
+        /// </summary>
+        /// <remarks>
+        /// Generate the && instruction.
+        /// <code>
+        ///  L_000d: ldloc.0  // Left
+        ///  L_000e: brfalse.s L_0013
+        ///  L_0010: ldloc.1  // Right
+        ///  L_0011: br.s L_0014
+        ///  L_0013: ldc.i4.0 
+        ///  L_0014: stloc.3  // Result 
+        /// </code>
+        /// </remarks> 
+        /// <param name="and">The and.</param>
+        public void VisitAndRight(And and)
+        {
+            Instructions.Add(Worker.Create(OpCodes.Br_S, GetJumpLabel(and.BranchId + 1)));
+            Instructions.Add(GetJumpLabel(and.BranchId));
+            Instructions.Add(Worker.Create(OpCodes.Ldc_I4_0));
+            Instructions.Add(GetJumpLabel(and.BranchId + 1));
+        }
+
+
+        #endregion
 
         #endregion
 
