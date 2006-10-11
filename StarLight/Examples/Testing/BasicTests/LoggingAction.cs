afe186f45b694bf0e2b78f22f26b8c0e98fdf9c6
Arjan de Roo <>
2006-10-11 08:37:24 +0000

    Added DefaultWeaveStrategy + changed testing example

---
+++ StarLight/Examples/Testing/BasicTests/LoggingAction.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Composestar.StarLight.ContextInfo.FilterTypes;
+
+namespace BasicTests
+{
+    [FilterActionAttribute("LoggingAction", FilterFlowBehaviour.Continue, MessageSubstitutionBehaviour.Original)]
+    class LoggingAction : FilterAction
+    {
+        public override void Execute(Composestar.StarLight.ContextInfo.JoinPointContext context)
+        {
+            Console.WriteLine("Log : " + context.MethodName);
+        }
+    }
+}
--- StarLight/Source/ILWeaver/Cecil/WeaveStrategy/DefaultWeaveStrategy.cs
+++ StarLight/Source/ILWeaver/Cecil/WeaveStrategy/DefaultWeaveStrategy.cs
@@ -8,6 +8,7 @@ using Mono.Cecil.Cil;
 
 using Composestar.StarLight.CoreServices.Exceptions;
 
+using Composestar.Repository.LanguageModel;
 using Composestar.Repository.LanguageModel.Inlining;
 
 using Composestar.StarLight.ContextInfo;
@@ -43,27 +44,31 @@ namespace Composestar.StarLight.ILWeaver
         {
             MethodReference methodToCall;
 
-            // Create FilterAction object:
-            //filterAction.
-
             // Get the methodReference
             MethodReference methodReference = (MethodReference) originalCall;
             TypeDefinition parentType = CecilUtilities.ResolveTypeDefinition(methodReference.DeclaringType);
 
-            // Get method to call
-            methodToCall = GetMethodToCall(visitor, filterAction, parentType);
-            if(methodToCall == null)
-            {
-                throw new ILWeaverException(String.Format(CultureInfo.CurrentCulture,
-                        Properties.Resources.AdviceMethodNotFound, filterAction.Selector, filterAction.Target));
-            }
-
-
             // Create JoinPointContext
             VariableDefinition jpcVar = WeaveStrategyUtilities.CreateJoinPointContext(visitor, methodReference, true);
 
-            // Do the advice-call
-            CallAdvice(visitor, filterAction, parentType, methodToCall, jpcVar);
+            // Create FilterAction object:
+            TypeElement typeElement = visitor.RepositoryAccess.GetTypeElement(filterAction.FullName);
+            TypeReference typeRef =
+                CecilUtilities.ResolveType(filterAction.FullName, typeElement.Assembly, null);
+            TypeDefinition typeDef =
+                CecilUtilities.ResolveTypeDefinition(typeRef);
+            MethodDefinition constructor = typeDef.Constructors.GetConstructor(false, new Type[0]);
+            visitor.Instructions.Add(visitor.Worker.Create(OpCodes.Newobj, constructor));
+
+            // Get method to call
+            methodToCall = typeDef.Methods.GetMethod("Execute")[0];
+
+            // Load the JoinPointObject as the parameter
+            visitor.Instructions.Add(visitor.Worker.Create(OpCodes.Ldloc, jpcVar));
+
+            // Do the call
+            // We can safely emit a callvirt here. The JITter will make the right call.
+            visitor.Instructions.Add(visitor.Worker.Create(OpCodes.Callvirt, methodToCall));
 
             // Restore the JoinPointContext:
             WeaveStrategyUtilities.RestoreJoinPointContext(visitor, methodReference, jpcVar, true);
@@ -71,80 +76,6 @@ namespace Composestar.StarLight.ILWeaver
 
             // Add nop to enable debugging
             visitor.Instructions.Add(visitor.Worker.Create(OpCodes.Nop));
-        }
-
-        /// <summary>
-        /// Weaves the call to the advice
-        /// </summary>
-        /// <param name="filterAction">The filteraction</param>
-        /// <param name="parentType">The type containing the original method</param>
-        /// <param name="methodToCall">The advice method</param>
-        /// <param name="jpcVar">The local variable containing the JoinPointContext</param>
-        private void CallAdvice(CecilInliningInstructionVisitor visitor,
-            FilterAction filterAction, TypeDefinition parentType, MethodReference methodToCall,
-            VariableDefinition jpcVar)
-        {
-            // Place target on the stack:
-            if(methodToCall.HasThis)
-            {
-                if(filterAction.Target.Equals(FilterAction.INNER_TARGET) ||
-                    filterAction.Target.Equals(FilterAction.SELF_TARGET))
-                {
-                    visitor.Instructions.Add(visitor.Worker.Create(OpCodes.Ldarg, visitor.Method.This));
-                }
-                else
-                {
-                    FieldDefinition target = parentType.Fields.GetField(filterAction.Target);
-                    if(target == null)
-                    {
-                        throw new ILWeaverException(String.Format(CultureInfo.CurrentCulture,
-                            Properties.Resources.FieldNotFound, filterAction.Target));
-                    }
-
-                    visitor.Instructions.Add(visitor.Worker.Create(OpCodes.Ldarg, visitor.Method.This));
-                    visitor.Instructions.Add(visitor.Worker.Create(OpCodes.Ldfld, target));
-                }
-            }
-
-            // Load the JoinPointObject as the parameter
-            visitor.Instructions.Add(visitor.Worker.Create(OpCodes.Ldloc, jpcVar));
-
-            // We can safely emit a callvirt here. The JITter will make the right call.
-            visitor.Instructions.Add(visitor.Worker.Create(OpCodes.Callvirt, methodToCall));
-
-        }
-
-
-        private Type[] m_JpcTypes = new Type[1] { typeof(JoinPointContext) };
-
-        /// <summary>
-        /// Returns the MethodReference to the advice method
-        /// </summary>
-        /// <param name="filterAction">The filteraction</param>
-        /// <param name="parentType">The type containing the original method</param>
-        /// <returns>The MethodReference to the advice method</returns>
-        private MethodReference GetMethodToCall(CecilInliningInstructionVisitor visitor,
-            FilterAction filterAction, TypeDefinition parentType)
-        {
-            if(filterAction.Target.Equals(FilterAction.INNER_TARGET) ||
-                filterAction.Target.Equals(FilterAction.SELF_TARGET))
-            {
-                return CecilUtilities.ResolveMethod(parentType, filterAction.Selector, m_JpcTypes);
-            }
-            else
-            {
-                FieldDefinition target = parentType.Fields.GetField(filterAction.Target);
-                if(target == null)
-                {
-                    throw new ILWeaverException(String.Format(CultureInfo.CurrentCulture,
-                        Properties.Resources.FieldNotFound, filterAction.Target));
-                }
-
-
-                MethodDefinition md = CecilUtilities.ResolveMethod(target.FieldType, filterAction.Selector, m_JpcTypes);
-
-                return visitor.TargetAssemblyDefinition.MainModule.Import(md);
-            }
         }
     }
 }
