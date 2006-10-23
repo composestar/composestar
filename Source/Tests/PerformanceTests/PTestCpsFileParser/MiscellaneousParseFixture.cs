7229e14415ed1fd6af56a86d6ca86983194f7127
Sverre Boschman <sboschman@users.noreply.github.com>
2006-10-23 07:13:45 +0000

    Updated performance and unit tests

--- Source/Tests/PerformanceTests/PTestCpsFileParser/MiscellaneousParseFixture.cs
+++ Source/Tests/PerformanceTests/PTestCpsFileParser/MiscellaneousParseFixture.cs
@@ -1,3 +1,4 @@
+#region Using directives
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
@@ -6,6 +7,7 @@ using NTime.Framework;
 
 using Composestar.StarLight.CoreServices;
 using Composestar.StarLight.CpsParser;
+#endregion
 
 namespace Composestar.StarLight.CpsParser.Tests.PerformanceTests
 {
@@ -17,101 +19,81 @@ namespace Composestar.StarLight.CpsParser.Tests.PerformanceTests
 
         }
 
-        [TimerFixtureSetUp]
-        public override void GlobalSetUp()
-        {
-            base.GlobalSetUp();
-
-            // Miscellaneous concerns
-            Concerns.Add("Miscellaneous_BulkUpdates", TestPath + "Concerns\\BulkUpdates.cps");
-            Concerns.Add("Miscellaneous_Credit", TestPath + "Concerns\\CreditConcern.cps");
-            Concerns.Add("Miscellaneous_InventoryObserver", TestPath + "Concerns\\InventoryObserver.cps");
-            Concerns.Add("Miscellaneous_JukeboxFrame", TestPath + "Concerns\\JukeboxFrameConcern.cps");
-            Concerns.Add("Miscellaneous_Law", TestPath + "Concerns\\Law.cps");
-            Concerns.Add("Miscellaneous_Logging", TestPath + "Concerns\\Logging.cps");
-            Concerns.Add("Miscellaneous_Money", TestPath + "Concerns\\Money.cps");
-            Concerns.Add("Miscellaneous_Party", TestPath + "Concerns\\Party.cps");
-            Concerns.Add("Miscellaneous_Platypus", TestPath + "Concerns\\Platypus.cps");
-            Concerns.Add("Miscellaneous_Playlist", TestPath + "Concerns\\PlaylistConcern.cps");
-            Concerns.Add("Miscellaneous_VenusFlyTrap", TestPath + "Concerns\\VenusFlyTrap.cps");
-
-        }
-
         #region Miscellaneous concerns
         [TimerDurationTest(50, Unit = TimePeriod.Millisecond)]
         public void Parse_Miscellaneous_BulkUpdates()
         {
-            CpsFileParser cfp = CreateParser(Concerns["Miscellaneous_BulkUpdates"]);
+            CpsFileParser cfp = CreateParser(TestPath + "Concerns\\BulkUpdates.cps");
             cfp.Parse();
         }
 
         [TimerDurationTest(50, Unit = TimePeriod.Millisecond)]
         public void Parse_Miscellaneous_Credit()
         {
-            CpsFileParser cfp = CreateParser(Concerns["Miscellaneous_Credit"]);
+            CpsFileParser cfp = CreateParser(TestPath + "Concerns\\CreditConcern.cps");
             cfp.Parse();
         }
 
         [TimerDurationTest(50, Unit = TimePeriod.Millisecond)]
         public void Parse_Miscellaneous_InventoryObserver()
         {
-            CpsFileParser cfp = CreateParser(Concerns["Miscellaneous_InventoryObserver"]);
+            CpsFileParser cfp = CreateParser(TestPath + "Concerns\\InventoryObserver.cps");
             cfp.Parse();
         }
 
         [TimerDurationTest(50, Unit = TimePeriod.Millisecond)]
         public void Parse_Miscellaneous_JukeboxFrame()
         {
-            CpsFileParser cfp = CreateParser(Concerns["Miscellaneous_JukeboxFrame"]);
+            CpsFileParser cfp = CreateParser(TestPath + "Concerns\\JukeboxFrameConcern.cps");
             cfp.Parse();
         }
 
         [TimerDurationTest(50, Unit = TimePeriod.Millisecond)]
         public void Parse_Miscellaneous_Law()
         {
-            CpsFileParser cfp = CreateParser(Concerns["Miscellaneous_Law"]);
+            CpsFileParser cfp = CreateParser(TestPath + "Concerns\\Law.cps");
             cfp.Parse();
         }
 
         [TimerDurationTest(50, Unit = TimePeriod.Millisecond)]
         public void Parse_Miscellaneous_Logging()
         {
-            CpsFileParser cfp = CreateParser(Concerns["Miscellaneous_Logging"]);
+            CpsFileParser cfp = CreateParser(TestPath + "Concerns\\Logging.cps");
             cfp.Parse();
         }
         
         [TimerDurationTest(50, Unit = TimePeriod.Millisecond)]
         public void Parse_Miscellaneous_Money()
         {
-            CpsFileParser cfp = CreateParser(Concerns["Miscellaneous_Money"]);
+            CpsFileParser cfp = CreateParser(TestPath + "Concerns\\Money.cps");
             cfp.Parse();
         }
 
         [TimerDurationTest(50, Unit = TimePeriod.Millisecond)]
         public void Parse_Miscellaneous_Party()
         {
-            CpsFileParser cfp = CreateParser(Concerns["Miscellaneous_Party"]);
+            CpsFileParser cfp = CreateParser(TestPath + "Concerns\\Party.cps");
             cfp.Parse();
         }
 
         [TimerDurationTest(50, Unit = TimePeriod.Millisecond)]
         public void Parse_Miscellaneous_Platypus()
         {
-            CpsFileParser cfp = CreateParser(Concerns["Miscellaneous_Platypus"]);
+            CpsFileParser cfp = CreateParser(TestPath + "Concerns\\Platypus.cps");
             cfp.Parse();
         }
 
         [TimerDurationTest(50, Unit = TimePeriod.Millisecond)]
         public void Parse_Miscellaneous_Playlist()
         {
-            CpsFileParser cfp = CreateParser(Concerns["Miscellaneous_Playlist"]);
+            CpsFileParser cfp = CreateParser(TestPath + "Concerns\\PlaylistConcern.cps");
             cfp.Parse();
         }
 
         [TimerDurationTest(50, Unit = TimePeriod.Millisecond)]
         public void Parse_Miscellaneous_VenusFlyTrap()
         {
-            CpsFileParser cfp = CreateParser(Concerns["Miscellaneous_VenusFlyTrap"]);
+            CpsFileParser cfp = CreateParser(TestPath + "Concerns\\VenusFlyTrap.cps");
             cfp.Parse();
         }
         #endregion
