b882a1273485f1ea1660aafeb7d770d8b2881818
Roy Spenkelink <>
2006-10-30 15:41:12 +0000

    factored code.

--- Java/Plugin/Eclipse/src/ComposestarEclipsePlugin/Java/MasterManager.java
+++ Java/Plugin/Eclipse/src/ComposestarEclipsePlugin/Java/MasterManager.java
@@ -3,6 +3,7 @@ package ComposestarEclipsePlugin.Java;
 import ComposestarEclipsePlugin.Core.BuildConfiguration.BuildConfigurationManager;
 import ComposestarEclipsePlugin.Core.Debug;
 import ComposestarEclipsePlugin.Core.Utils.CommandLineExecutor;
+import ComposestarEclipsePlugin.Core.Utils.FileUtils;
 
 import org.eclipse.jface.dialogs.IDialogSettings;
 
@@ -11,21 +12,60 @@ import org.eclipse.jface.dialogs.IDialogSettings;
  */
 public class MasterManager {
 
+	/**
+	 * The instance. 
+	 */
+	private static MasterManager Instance = null;
+	
+	/**
+	 * If true the compilation has ended succesfully
+	 */
 	public boolean completed = false;
-		
+	
+	/**
+	 * The main class to execute
+	 */
+	protected String mainClass = "Composestar.Java.MASTER.JavaMaster";
+	
+	/**
+	 * The JVM options
+	 */
+	protected String jvmOptions = "";
+	
+	/**
+	 * If true the output is logged to a file.
+	 */
+	public boolean logOutput = false;
+	
+	/**
+	 * The name of the logfile.
+	 */
+	protected String logFile = "buildlog.txt";
+	
 	public MasterManager(){
 		
+	}
+	
+	public static MasterManager getInstance()
+	{
+		if(Instance==null)
+		{
+			Instance = new MasterManager();
+		}
+		return Instance;
 	}
 	
 	public void run(IDialogSettings settings) {
 		
 		BuildConfigurationManager bcmanager = BuildConfigurationManager.instance();
-				
+		String basePath = FileUtils.getDirectoryPart(bcmanager.buildconfigFile);		
 		try {
-			String jvmOptions = "";
 			String classPath = settings.get("classpath");
-			String mainClass = "Composestar.Java.MASTER.JavaMaster";
 			String command = "java.exe " + jvmOptions + " -cp \"" + classPath + "\" " + mainClass + " " + "\"" +bcmanager.buildconfigFile + "\"";
+			if(logOutput)
+			{
+				command += " > "+basePath+java.io.File.separator+logFile;
+			}
 			CommandLineExecutor cmdExec = new CommandLineExecutor();
             int result = cmdExec.exec(  "call " + command);
             
