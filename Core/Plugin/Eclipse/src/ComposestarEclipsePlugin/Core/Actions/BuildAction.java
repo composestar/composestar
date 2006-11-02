745d92b603b5a8cbcc2c47544bfda9fd6ea28e49
Roy Spenkelink <>
2006-11-02 09:03:18 +0000

    factored code; builtOk public access.

--- Core/Plugin/Eclipse/src/ComposestarEclipsePlugin/Core/Actions/BuildAction.java
+++ Core/Plugin/Eclipse/src/ComposestarEclipsePlugin/Core/Actions/BuildAction.java
@@ -35,63 +35,83 @@ public class BuildAction {
 	protected Path composestarPath = new Path();
 	protected Project projectConfig = null;
 	protected String language;
-	protected boolean builtOk = true;
+	
+	protected String buildPath = "";
+	protected String outputPath = "";
+	public boolean builtOk = true;
 		
 	/**
 	 * The constructor.
 	 */
-	public BuildAction() {
+	public BuildAction() 
+	{
 		buildConfig = BuildConfigurationManager.instance();
+		projectConfig = new Project();
 	}
 
-	public void dispose() {
+	public void dispose() 
+	{
 	}
 	
-	public static IProject[] extractProjects(Object[] selection) {
+	public static IProject[] extractProjects(Object[] selection) 
+	{
 		HashSet projects = new HashSet();
 			
-		for (int i = 0; i < selection.length; i++) {
-			if (selection[i] instanceof IResource) {
+		for (int i = 0; i < selection.length; i++) 
+		{
+			if (selection[i] instanceof IResource) 
+			{
 				projects.add(((IResource)selection[i]).getProject());
-			} else if (selection[i] instanceof IAdaptable){
+			} 
+			else if (selection[i] instanceof IAdaptable)
+			{
 				IAdaptable adaptable = (IAdaptable)selection[i];
 				IResource resource = (IResource)adaptable.getAdapter(IResource.class);
 				if (resource != null)
+				{	
 					projects.add(resource.getProject());
+				}
 			}
 		}
 		return (IProject[]) projects.toArray(new IProject[projects.size()]);
 	}
 	
-	public void init(IWorkbenchWindow window) {
+	public void init(IWorkbenchWindow window) 
+	{
 		this.window = window;
 	}
 	
-	public void selectionChanged(IAction action, ISelection selection) {
-	
-		if (selection == null || selection.isEmpty() || (!(selection instanceof IStructuredSelection))){
+	public void selectionChanged(IAction action, ISelection selection) 
+	{
+		if (selection == null || selection.isEmpty() || (!(selection instanceof IStructuredSelection)))
+		{
 			selectedProjects = NO_PROJECTS;
 		}
-		else{
+		else
+		{
 			selectedProjects = extractProjects(((IStructuredSelection)selection).toArray());
 		}
 		action.setEnabled(selectedProjects.length > 0); //&& !ResourcesPlugin.getWorkspace().isAutoBuilding());
 	}
 	
-	public void setConcernSources(HashSet list, IPath workspace) {
+	public void setConcernSources(HashSet list, IPath workspace) 
+	{
 		Iterator l=list.iterator();
 		workspace=workspace.removeLastSegments(1);
-		while(l.hasNext()){
+		while(l.hasNext())
+		{
 			IPath test = (IPath)l.next();
 			test=workspace.append(test);
 			BuildConfigurationManager.instance().setConcernSources(FileUtils.fixFilename(test.toOSString()));
 		}
 	}
 	
-	public void setSources(HashSet list, IPath workspace){
+	public void setSources(HashSet list, IPath workspace)
+	{
 		Iterator l=list.iterator();
 		workspace=workspace.removeLastSegments(1);
-		while(l.hasNext()){
+		while(l.hasNext())
+		{
 			IPath test = (IPath)l.next();
 			test=workspace.append(test);
 			projectConfig.addSource(FileUtils.fixFilename(test.toOSString()));
