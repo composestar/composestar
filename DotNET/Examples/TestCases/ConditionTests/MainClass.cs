797469abb4c5a417f3ea480e5cdaf7ab0e620cf6
Michiel Hendriks <git@elmuerte.com>
2006-11-02 10:59:40 +0000

    seperated conditiontests from basic test

---
+++ DotNET/Examples/TestCases/ConditionTests/MainClass.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ConditionTests
+{
+	class MainClass
+	{
+		static void Main(string[] args)
+		{
+			ConditionClass conds = new ConditionClass();
+			conds.func1();
+			conds.func2();
+			conds.func3();
+			conds.func4();
+		}
+	}
+}
