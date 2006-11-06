7901767c6550b56ec6892661ad495040ce640200
Roy Spenkelink <>
2006-11-06 14:53:39 +0000

    factored code. Added supertype Action.

--- Core/Plugin/Eclipse/src/ComposestarEclipsePlugin/Core/Actions/BuildAction.java
+++ Core/Plugin/Eclipse/src/ComposestarEclipsePlugin/Core/Actions/BuildAction.java
@@ -1,120 +1,157 @@
 package ComposestarEclipsePlugin.Core.Actions;
 
-import ComposestarEclipsePlugin.Core.BuildConfiguration.*;
-import ComposestarEclipsePlugin.Core.Utils.FileUtils;
-
 import java.util.HashSet;
 import java.util.Iterator;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IWorkbenchWindow;
 
-/** 
- *  TODO: Add support for building multiple projects.
+import ComposestarEclipsePlugin.Core.ComposestarEclipsePluginPlugin;
+import ComposestarEclipsePlugin.Core.Debug;
+import ComposestarEclipsePlugin.Core.BuildConfiguration.*;
+import ComposestarEclipsePlugin.Core.Utils.FileUtils;
+
+/**
+ * Each language should extends this class. (e.g. JavaBuildAction ,
+ * CBuildAction)
  */
+public class BuildAction extends Action
+{
 
-public class BuildAction {
-	
-	protected IWorkbenchWindow window;
-	protected static final IProject[] NO_PROJECTS = new IProject[0];
-	protected IProject[] selectedProjects = NO_PROJECTS;
-	protected IProject project = null;
-	protected IPath projectLocation = null;
-	protected IResource[] projectMembers= null;
-	
+	/**
+	 * Used to add information to the build configuration file.
+	 */
 	protected BuildConfigurationManager buildConfig;
+
+	/**
+	 * The project to be built.
+	 */
+	protected IProject project;
+
+	/**
+	 * The location of the project.
+	 */
+	protected IPath projectLocation;
+
+	/**
+	 * The concerns found in the project. The HashSet contains filenames
+	 * (Strings).
+	 */
 	protected HashSet concerns = new HashSet();
+
+	/**
+	 * The sources found in the project. The HashSet contains filenames
+	 * (Strings).
+	 */
 	protected HashSet sources = new HashSet();
-	protected Path basePath = new Path();
-	protected Path composestarPath = new Path();
-	protected Project projectConfig = null;
-	protected String language;
-	
+
+	/**
+	 * The project to be built. Contains only information that is needed in the
+	 * build configuration file.
+	 */
+	protected Project projectConfig;
+
+	/**
+	 * Build path. ( obj/ )
+	 */
 	protected String buildPath = "";
+
+	/**
+	 * Output path. Location of the compiled sources.
+	 */
 	protected String outputPath = "";
-	public boolean builtOk = true;
-		
+
+	/**
+	 * If false, some error occured while building a project. Public access
+	 * needed for headless mode (see TestBuild)
+	 */
+	public boolean builtOk = false;
+
 	/**
 	 * The constructor.
 	 */
-	public BuildAction() 
+	public BuildAction()
 	{
 		buildConfig = BuildConfigurationManager.instance();
 		projectConfig = new Project();
 	}
 
-	public void dispose() 
+	/**
+	 * Adds the concernsources to the build configuration file.
+	 * 
+	 * @param list - a list of concernsources
+	 * @param projectBase - base location of project
+	 */
+	public void setConcernSources(HashSet list, IPath projectBase)
 	{
-	}
-	
-	public static IProject[] extractProjects(Object[] selection) 
-	{
-		HashSet projects = new HashSet();
-			
-		for (int i = 0; i < selection.length; i++) 
+		Iterator l = list.iterator();
+		projectBase = projectBase.removeLastSegments(1);
+		while (l.hasNext())
 		{
-			if (selection[i] instanceof IResource) 
-			{
-				projects.add(((IResource)selection[i]).getProject());
-			} 
-			else if (selection[i] instanceof IAdaptable)
-			{
-				IAdaptable adaptable = (IAdaptable)selection[i];
-				IResource resource = (IResource)adaptable.getAdapter(IResource.class);
-				if (resource != null)
-				{	
-					projects.add(resource.getProject());
-				}
-			}
-		}
-		return (IProject[]) projects.toArray(new IProject[projects.size()]);
-	}
-	
-	public void init(IWorkbenchWindow window) 
-	{
-		this.window = window;
-	}
-	
-	public void selectionChanged(IAction action, ISelection selection) 
-	{
-		if (selection == null || selection.isEmpty() || (!(selection instanceof IStructuredSelection)))
-		{
-			selectedProjects = NO_PROJECTS;
-		}
-		else
-		{
-			selectedProjects = extractProjects(((IStructuredSelection)selection).toArray());
-		}
-		action.setEnabled(selectedProjects.length > 0); //&& !ResourcesPlugin.getWorkspace().isAutoBuilding());
-	}
-	
-	public void setConcernSources(HashSet list, IPath workspace) 
-	{
-		Iterator l=list.iterator();
-		workspace=workspace.removeLastSegments(1);
-		while(l.hasNext())
-		{
-			IPath test = (IPath)l.next();
-			test=workspace.append(test);
-			BuildConfigurationManager.instance().setConcernSources(FileUtils.fixFilename(test.toOSString()));
+			IPath path = (IPath) l.next();
+			path = projectBase.append(path);
+			BuildConfigurationManager.instance().setConcernSources(FileUtils.fixFilename(path.toOSString()));
 		}
 	}
-	
-	public void setSources(HashSet list, IPath workspace)
+
+	/**
+	 * Adds the sources to the build configuration file.
+	 * 
+	 * @param list - a list of sources
+	 * @param projectBase - base location of project
+	 */
+	public void setSources(HashSet list, IPath projectBase)
 	{
-		Iterator l=list.iterator();
-		workspace=workspace.removeLastSegments(1);
-		while(l.hasNext())
+		Iterator l = list.iterator();
+		projectBase = projectBase.removeLastSegments(1);
+		while (l.hasNext())
 		{
-			IPath test = (IPath)l.next();
-			test=workspace.append(test);
-			projectConfig.addSource(FileUtils.fixFilename(test.toOSString()));
+			IPath path = (IPath) l.next();
+			path = projectBase.append(path);
+			projectConfig.addSource(FileUtils.fixFilename(path.toOSString()));
 		}
+	}
+
+	/**
+	 * Adds the paths to the build configuration file.
+	 */
+	public void setPaths()
+	{
+		if (selectedProjects == NO_PROJECTS)
+		{
+			// no projects selected
+			Debug.instance().Log("No projects selected!", Debug.MSG_ERROR);
+		}
+		
+		// buildPath
+		buildPath = FileUtils.fixFilename(projectLocation.toOSString() + "/obj/");
+
+		BuildConfigurationManager buildConfig = BuildConfigurationManager.instance();
+		buildConfig.setOutputPath(outputPath);
+
+		projectConfig.addProperty("basePath", FileUtils.fixFilename(projectLocation.toOSString() + "/"));
+		projectConfig.addProperty("outputPath", outputPath);
+		projectConfig.addProperty("buildPath", buildPath);
+
+		// more paths
+		Path p = new Path();
+		p.setName("Base");
+		p.setPath(FileUtils.fixFilename(projectLocation.toOSString() + "/"));
+		buildConfig.addPath(p);
+
+		p = new Path();
+		p.setName("Composestar");
+		p.setPath(ComposestarEclipsePluginPlugin.getAbsolutePath("/"));
+		buildConfig.addPath(p);
+
+		p = new Path();
+		p.setName("EmbeddedSources");
+		p.setPath("embedded/");
+		buildConfig.addPath(p);
+
+		p = new Path();
+		p.setName("Dummy");
+		p.setPath("dummies/");
+		buildConfig.addPath(p);
 	}
 }
