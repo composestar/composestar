8d90fd93a2d087c8c27806ec9c767d164d02e2d7
Michiel Hendriks <git@elmuerte.com>
2006-11-12 11:02:13 +0000

    initial code for walking the dispatch graph

--- Core/Compiletime/src/Composestar/Core/DIGGER/NOBBIN.java
+++ Core/Compiletime/src/Composestar/Core/DIGGER/NOBBIN.java
@@ -10,18 +10,27 @@
 
 package Composestar.Core.DIGGER;
 
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
 
 import Composestar.Core.DIGGER.Graph.AbstractConcernNode;
 import Composestar.Core.DIGGER.Graph.ConcernNode;
+import Composestar.Core.DIGGER.Graph.CondMatchEdge;
+import Composestar.Core.DIGGER.Graph.Edge;
 import Composestar.Core.DIGGER.Graph.Graph;
+import Composestar.Core.DIGGER.Graph.LambdaEdge;
+import Composestar.Core.DIGGER.Graph.Node;
+import Composestar.Core.DIGGER.Graph.SubstitutionEdge;
+import Composestar.Core.DIGGER.Walker.Message;
+import Composestar.Core.DIGGER.Walker.MessageGenerator;
 import Composestar.Core.Exception.ModuleException;
 import Composestar.Core.Master.Config.Configuration;
+import Composestar.Utils.Debug;
 
 /**
- * A NOBBIN will walk the paths created by DIGGER
+ * A NOBBIN will walk the paths created by DIGGER in order to produce a set of
+ * resulting messages for an incomming message. The goal is to figure out the actual
+ * destination of a message through all concerns.
  * 
  * @author Michiel Hendriks
  */
@@ -32,17 +41,15 @@ public class NOBBIN
 	/**
 	 * Traverse the graph up to this number of concerns
 	 */
-	protected int maxDepth;
+	protected int maxDepth = 5;
 
-	protected Map signatures;
+	MessageGenerator gen = new MessageGenerator();
 
 	public NOBBIN(Graph inGraph)
 	{
-		signatures = new HashMap();
 		graph = inGraph;
-		maxDepth = Configuration.instance().getModuleProperty(DIGGER.MODULE_NAME, "maxdepth", 5); // just
-		// a
-		// number
+		maxDepth = Configuration.instance().getModuleProperty(DIGGER.MODULE_NAME, "maxdepth", 5);
+		// just a number
 	}
 
 	/**
@@ -71,6 +78,96 @@ public class NOBBIN
 	 */
 	public void walk(ConcernNode concernNode) throws ModuleException
 	{
+		Debug.out(Debug.MODE_DEBUG, "Walking concern " + concernNode.getLabel(), DIGGER.MODULE_NAME);
+		/*
+		 * Walk through the graph from the concernNode and spawn a new Message
+		 * for each FilterElement then walk the whole graph using that message.
+		 * Creating new messages on the fly.
+		 */
 
+		walk(concernNode.getOutputFilters(), null, 0);
+		walk(concernNode.getInputFilters(), null, 0);
+
+		// Node n = concernNode.getOutputFilters();
+		// while (n != null)
+		// {
+		// Debug.out(Debug.MODE_DEBUG, "Walking node " + n.getLabel(),
+		// DIGGER.MODULE_NAME);
+		// Iterator edges = n.getOutgoingEdges();
+		// n = null;
+		// while (edges.hasNext())
+		// {
+		// Edge e = (Edge) edges.next();
+		// if (e instanceof CondMatchEdge)
+		// {
+		// Message msg = gen.create((CondMatchEdge) e);
+		// walk(e.getDestination(), msg);
+		// }
+		// else if (e instanceof LambdaEdge)
+		// {
+		// n = e.getDestination();
+		// }
+		// }
+		// }
+		//		
+		// n = concernNode.getInputFilters();
+		// while (n != null)
+		// {
+		// Debug.out(Debug.MODE_DEBUG, "Walking node " + n.getLabel(),
+		// DIGGER.MODULE_NAME);
+		// Iterator edges = n.getOutgoingEdges();
+		// n = null;
+		// while (edges.hasNext())
+		// {
+		// Edge e = (Edge) edges.next();
+		// if (e instanceof CondMatchEdge)
+		// {
+		// Message msg = gen.create((CondMatchEdge) e);
+		// walk(e.getDestination(), msg);
+		// }
+		// else if (e instanceof LambdaEdge)
+		// {
+		// n = e.getDestination();
+		// }
+		// }
+		// }
+	}
+
+	public void walk(Node node, Message msg, int curDepth) throws ModuleException
+	{
+		while (node != null)
+		{
+			Debug.out(Debug.MODE_DEBUG, "Walking node " + node.getLabel(), DIGGER.MODULE_NAME);
+			Iterator edges = node.getOutgoingEdges();
+			node = null;
+			while (edges.hasNext())
+			{
+				Edge e = (Edge) edges.next();
+				if (e instanceof CondMatchEdge)
+				{
+					CondMatchEdge cme = (CondMatchEdge) e;
+					if (msg != null)
+					{
+						if (!msg.matches(cme)) continue;
+
+						walk(e.getDestination(), msg, curDepth);
+					}
+					else
+					{
+						List messages = gen.create(cme);
+
+					}
+				}
+				else if (e instanceof SubstitutionEdge)
+				{
+					Message newMsg = gen.xform(msg, (SubstitutionEdge) e);
+					walk(e.getDestination(), newMsg, curDepth++);
+				}
+				else if (e instanceof LambdaEdge)
+				{
+					node = e.getDestination();
+				}
+			}
+		}
 	}
 }
