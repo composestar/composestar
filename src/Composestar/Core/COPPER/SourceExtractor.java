67040f7ad5bbf585a5b12d380bb2c7bb222aeed3
Marcus Klimstra <>
2006-11-17 15:31:46 +0000

    updated ant to use the new antlr task.

--- src/Composestar/Core/COPPER/SourceExtractor.java
+++ src/Composestar/Core/COPPER/SourceExtractor.java
@@ -9,65 +9,38 @@
  */
 package Composestar.Core.COPPER;
 
-import java.io.*;
-
-import Composestar.Utils.*;
-import antlr.ANTLRException;
-
+import Composestar.Core.Exception.ModuleException;
+import Composestar.Utils.Debug;
 
 /**
  * Extracts embedded source
  */
-public class SourceExtractor {
-  
-	public void extractSource() {
-	   if (COPPER.getParser().sourceIncluded) {
-	      String b = COPPER.getCpscontents();
-		   int endpos = b.lastIndexOf('}'); //Closing tag cps
-		   if(endpos > 0)
-		   {
-			   endpos = b.lastIndexOf('}',endpos -1); //Closing tag implementation by
-		   }
-		   if(endpos <= 0)
-		   {
-			   Debug.out(Debug.MODE_WARNING,"COPPER","Expecting closing '}' at end of cps file and after embedded source");
-			   Debug.out(Debug.MODE_WARNING,"COPPER","Ignoring embedded source");
-			   return;
-			}
-	      COPPER.setEmbeddedSource(b.substring(COPPER.getParser().startPos, endpos));
-	      Debug.out(Debug.MODE_DEBUG,"COPPER",COPPER.getEmbeddedSource());
-	   }
-  }
+public class SourceExtractor
+{
+	public static final String MODULE_NAME = "COPPER";
 
-  /**
-   * Gets the position of where the embedded source ends. This is done by reversing the cps file and
-   * using a special parser to find 'the beginning of the end'. The starting position is already known
-   * from the regular parsing.
-   */
-	/*
-  private int getEndPos(String b) {
-	 
-    CpsSrcPosLexer srclexer;
-    CpsSrcParser srcparser;
-    
-    try {
-      // We are parsing backwards so reverse!
-      b.reverse();
-      // Create parser and lexer
-      srclexer = new CpsSrcPosLexer(new StringReader(b.toString()));
-      srcparser = new CpsSrcParser(srclexer);
-      // Start parsing with the src method
-      srcparser.src();
-      // Reversed code is not so nice, so reverse it back :)
-      b.reverse();
-      return (srcparser.endPos);
-    } 
-    catch (ANTLRException e)
+	public void extractSource() throws ModuleException
 	{
-      e.printStackTrace();
-      return (-1);
-    }
-	  return b.lastIndexOf("}");
-  }
-  */
+		if (COPPER.getParser().sourceLang != null)
+		{
+			String b = COPPER.getCpscontents();
+
+			// find second-last index of '}'
+			int endpos = b.lastIndexOf('}'); // Closing tag of concern
+			if (endpos > 0)
+			{
+				endpos = b.lastIndexOf('}', endpos - 1); // Closing tag of implementation by
+			}
+
+			if (endpos <= 0)
+			{
+				throw new ModuleException("Expecting closing '}' at end of cps file and after embedded source", MODULE_NAME);
+			}
+
+			String es = b.substring(COPPER.getParser().startPos, endpos);
+			COPPER.setEmbeddedSource(es);
+			
+			Debug.out(Debug.MODE_DEBUG, MODULE_NAME, es);
+		}
+	}
 }
