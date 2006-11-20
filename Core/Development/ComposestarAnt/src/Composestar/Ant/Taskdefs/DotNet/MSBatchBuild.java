285e269293f6390bc8bba3e7f679b3219b77d9c4
Marcus Klimstra <>
2006-11-20 10:42:02 +0000

    ComposestarAnt: MSBatchBuild expanded with failOnError and target attributes.

--- Core/Development/ComposestarAnt/src/Composestar/Ant/Taskdefs/DotNet/MSBatchBuild.java
+++ Core/Development/ComposestarAnt/src/Composestar/Ant/Taskdefs/DotNet/MSBatchBuild.java
@@ -20,18 +20,28 @@ public final class MSBatchBuild extends BaseTask
 {
 	private final static String EXECUTABLE = "msbuild";
 	
+	private String target;
+	private boolean failOnError = false;
+	
 	public MSBatchBuild()
 	{
 		super();
 	}
 	
+	public void setTarget(String target)
+	{
+		this.target = target;
+	}
+	
+	public void setFailOnError(boolean failOnError)
+	{
+		this.failOnError = failOnError;
+	}
+	
 	public void execute() throws BuildException
-	{		
-		String cmd[] = new String[5];
-		cmd[0] = EXECUTABLE;
-		cmd[2] = "/t:ComposeStarGen";
-		cmd[3] = "/v:q";
-		cmd[4] = "/nologo";
+	{
+		validateAttributes();
+		String[] cmd = createCommand();
 		
 		ExecuteStreamHandler sh = new LogStreamHandler(this, Project.MSG_INFO, Project.MSG_WARN);		
 		Execute exec = new Execute(sh);
@@ -40,6 +50,9 @@ public final class MSBatchBuild extends BaseTask
 		try
 		{
 			List inputs = collectInputs();
+
+			log("Building " + inputs.size() + " projects/solutions", Project.MSG_INFO);
+
 			Iterator it = inputs.iterator();		
 			while (it.hasNext())
 			{
@@ -51,14 +64,35 @@ public final class MSBatchBuild extends BaseTask
 	
 				int result = exec.execute();			
 				if (Execute.isFailure(result))
-					log("! Failed ! Exitcode is " + result, Project.MSG_INFO);
-				
-				// TODO: add failOnError
+				{
+					if (failOnError)
+						throw new BuildException("MSBuild failed. Exitcode is " + result);
+					else
+						log("! Failed ! Exitcode is " + result, Project.MSG_INFO);
+				}
 			}
 		}
 		catch (IOException e)
 		{
 			throw new BuildException("Unable to execute command: " + e.getMessage());
 		}
+	}
+	
+	private void validateAttributes() throws BuildException
+	{
+		if (target == null)
+			throw new BuildException("target attribute is required");		
+	}
+	
+	private String[] createCommand()
+	{
+		String cmd[] = new String[5];
+		cmd[0] = EXECUTABLE;
+		cmd[1] = "<buildfile>";
+		cmd[2] = "/t:" + target;
+		cmd[3] = "/v:q";
+		cmd[4] = "/nologo";
+		
+		return cmd;
 	}
 }
