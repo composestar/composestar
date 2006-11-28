a3e765dce652dcd96c7b79bb991973e0d293c064
Michiel van Oudheusden <mivano@users.noreply.github.com>
2006-11-28 13:43:55 +0000

    Added start menu items to the setup. Help file, ARM and website are accessible from the start menu. Updated help file about MSBuild externsions.

--- StarLight/Source/VSIntegration/VisualStudio.LanguageServices/PrologFunctions/PrologFunction.cs
+++ StarLight/Source/VSIntegration/VisualStudio.LanguageServices/PrologFunctions/PrologFunction.cs
@@ -1,11 +1,48 @@
+#region License
+/*
+ * This file is part of Composestar project [http://composestar.sf.net].
+ * ComposeStar StarLight [http://janus.cs.utwente.nl:8000/twiki/bin/view/StarLight/WebHome]
+ * Copyright (C) 2003, University of Twente.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification,are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Twente nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+*/
+#endregion
+
+#region Using directives
+using Composestar.StarLight.VisualStudio.Babel;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
-using System.IO;
-
-using Composestar.StarLight.VisualStudio.Babel;
+#endregion
 
 namespace Composestar.StarLight.VisualStudio.LanguageServices.Prolog
 {
@@ -13,12 +50,16 @@ namespace Composestar.StarLight.VisualStudio.LanguageServices.Prolog
 	[Serializable]
 	public class PrologFunction : IComparable
 	{
+		/// <summary>
+		/// _function name
+		/// </summary>
 		private string _functionName;
 
 		/// <summary>
 		/// Gets or sets the name of the function.
 		/// </summary>
 		/// <value>The name of the function.</value>
+		/// <returns>String</returns>
 		[XmlAttribute]
 		public string FunctionName
 		{
@@ -32,12 +73,16 @@ namespace Composestar.StarLight.VisualStudio.LanguageServices.Prolog
 			}
 		}
 
+		/// <summary>
+		/// _description
+		/// </summary>
 		private string _description;
 
 		/// <summary>
 		/// Gets or sets the description.
 		/// </summary>
 		/// <value>The description.</value>
+		/// <returns>String</returns>
 		[XmlAttribute]
 		public string Description
 		{
@@ -51,6 +96,9 @@ namespace Composestar.StarLight.VisualStudio.LanguageServices.Prolog
 			}
 		}
 
+		/// <summary>
+		/// _parameters
+		/// </summary>
 		private List<PrologParameter> _parameters = new List<PrologParameter>();
 
 		/// <summary>
--- StarLight/Source/VSIntegration/VisualStudio.LanguageServices/UserSupplied/Resolver.cs
+++ StarLight/Source/VSIntegration/VisualStudio.LanguageServices/UserSupplied/Resolver.cs
@@ -1,3 +1,39 @@
+#region License
+/*
+ * This file is part of Composestar project [http://composestar.sf.net].
+ * ComposeStar StarLight [http://janus.cs.utwente.nl:8000/twiki/bin/view/StarLight/WebHome]
+ * Copyright (C) 2003, University of Twente.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification,are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Twente nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+*/
+#endregion
+
 /***************************************************************************
 
 Copyright (c) Microsoft Corporation. All rights reserved.
@@ -9,31 +45,45 @@ PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
 
 ***************************************************************************/
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Reflection;
+#region Using directives
+using Composestar.StarLight.VisualStudio.Babel;
+using Composestar.StarLight.VisualStudio.LanguageServices;
 using Composestar.StarLight.VisualStudio.LanguageServices.Prolog;
-using ErrorHandler = Microsoft.VisualStudio.ErrorHandler;
 using Microsoft.VisualStudio.Package;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.TextManager.Interop;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using ErrorHandler = Microsoft.VisualStudio.ErrorHandler;
 using VSConstants = Microsoft.VisualStudio.VSConstants;
-using Composestar.StarLight.VisualStudio.Babel;
-using Composestar.StarLight.VisualStudio.LanguageServices;
+#endregion
 
 namespace Composestar.StarLight.VisualStudio.Babel
 {
+	/// <summary>
+	/// Resolver
+	/// </summary>
 	public class Resolver : Babel.IASTResolver
 	{
 
+		/// <summary>
+		/// _prolog functions
+		/// </summary>
 		private static PrologFunctions _prologFunctions = PrologFunctions.LoadFunctions(); 
 
 
 		#region IASTResolver Members
 
+		/// <summary>
+		/// Types
+		/// </summary>
 		private List<Type> types = new List<Type>();
 
+		/// <summary>
+		/// Create resolver
+		/// </summary>
 		public Resolver()
 		{
 			if (types.Count == 0)
@@ -48,6 +98,9 @@ namespace Composestar.StarLight.VisualStudio.Babel
 			
 			}
 
+			 /// <summary>
+			 /// Expansions list
+			 /// </summary>
 			 //if (this.expansionsList == null)
 			 //{
 			 //    GetSnippets();
@@ -85,7 +138,10 @@ namespace Composestar.StarLight.VisualStudio.Babel
 			}
 		}
 
-		// Disable the "DoNotPassTypesByReference" warning.
+		/// <summary>
+		/// Disable the "DoNotPassTypesByReference" warning.
+		/// </summary>
+		/// <param name="declarations">Declarations</param>
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1045")]
 		public void AddSnippets(ref List<Babel.Declaration> declarations)
 		{
