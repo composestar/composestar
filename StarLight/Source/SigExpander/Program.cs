17e3598be1304180fda9a1e39c94ec79a3c002e4
Marcus Klimstra <>
2006-12-05 13:19:10 +0000

    -Added empty SigExpander project in IL Analyser and Weaver folder.

---
+++ StarLight/Source/SigExpander/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigExpander
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
