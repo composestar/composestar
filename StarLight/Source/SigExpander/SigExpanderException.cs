0760f203b65067b0bd0fc774379f05a495638435
Marcus Klimstra <>
2006-12-08 10:19:03 +0000

    Commit to make cc build :P

---
+++ StarLight/Source/SigExpander/SigExpanderException.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text;
+
+namespace Composestar.StarLight.SigExpander
+{
+	public class SigExpanderException : Exception
+	{
+		public SigExpanderException(string m)
+			: base(m)
+		{
+		}
+	}
+}
