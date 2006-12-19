f0e8d1f29a1c62780dd5af03d2447da77cdd3a9d
Rolf Huisman <>
2006-12-19 19:40:12 +0000

    Ok, now you can actualy give ant options

--- Core/build.sh
+++ Core/build.sh
@@ -2,4 +2,4 @@
 export ANT_HOME=./Development/Ant
 export JAVA_HOME=/usr/java
 export PATH=${PATH}:${ANT_HOME}/bin
-java -cp Development/Ant/lib/ant-launcher.jar  org.apache.tools.ant.launch.Launcher -buildfile Development/build_total.xml
+java -cp Development/Ant/lib/ant-launcher.jar  org.apache.tools.ant.launch.Launcher -buildfile Development/build_total.xml $@
