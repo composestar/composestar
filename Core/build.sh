6dc0d51b3d4f010bd9ce1ac4378cf8da98ed685b
Rolf Huisman <>
2006-12-19 19:18:44 +0000

    support compiling under solaris 10 sparc

---
+++ Core/build.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+export ANT_HOME=./Development/Ant
+export JAVA_HOME=/usr/java
+export PATH=${PATH}:${ANT_HOME}/bin
+java -cp Development/Ant/lib/ant-launcher.jar  org.apache.tools.ant.launch.Launcher -buildfile Development/build_total.xml
