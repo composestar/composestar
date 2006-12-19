1a167804350222004c4a6873fd4cd48dc08b883a
Rolf Huisman <>
2006-12-19 19:58:00 +0000

    test now crashes with same reports as under windows :)

--- Core/build.sh
+++ Core/build.sh
@@ -2,4 +2,5 @@
 export ANT_HOME=./Development/Ant
 export JAVA_HOME=/usr/java
 export PATH=${PATH}:${ANT_HOME}/bin
+export ECLIPSE_HOME=/usr/local/eclipse
 java -cp Development/Ant/lib/ant-launcher.jar  org.apache.tools.ant.launch.Launcher -buildfile Development/build_total.xml $@
