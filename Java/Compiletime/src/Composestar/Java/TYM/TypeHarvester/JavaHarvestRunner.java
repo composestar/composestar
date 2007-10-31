94e0a2cfc3804f2139c401ce9e07da266980d310
W.K. Havinga <>
2007-10-31 15:37:00 +0000

    Rewriting this because seriously, WTF

--- Java/Compiletime/src/Composestar/Java/TYM/TypeHarvester/JavaHarvestRunner.java
+++ Java/Compiletime/src/Composestar/Java/TYM/TypeHarvester/JavaHarvestRunner.java
@@ -3,11 +3,15 @@ package Composestar.Java.TYM.TypeHarvester;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import Composestar.Core.Exception.ModuleException;
 import Composestar.Core.Master.CommonResources;
@@ -23,96 +27,55 @@ public class JavaHarvestRunner implements HarvestRunner
 	public static final String CLASS_MAP = "ClassMap";  
 	
 	/**
+	 * Iterates over all elements in a JARfile, loading those that end in .class
+	 * (This is the only way to load classes in a JARfile without knowing their names in advance)
+	 */
+	public HashMap<String,Class> harvest(URL jarURL) throws URISyntaxException, ClassNotFoundException, IOException
+	{
+		ClassLoader classLoader = new URLClassLoader(new URL[] { jarURL });
+		HashMap<String,Class> map = new HashMap<String,Class>();
+		File jar = new File(jarURL.toURI());
+		JarFile jarFile = new JarFile(jar);
+
+		Enumeration<JarEntry> entries = jarFile.entries();
+		while (entries.hasMoreElements())
+		{
+			JarEntry entry = entries.nextElement();
+			String classFileName = entry.getName();
+			if (classFileName.endsWith(".class"))
+			{
+                String className = classFileName.replace('/', '.').substring(0, classFileName.lastIndexOf('.'));
+                Class c = classLoader.loadClass(className);
+                map.put(c.getName(), c);
+			}
+		}
+		return map;
+	}
+	
+	/**
 	 * Module run method.
 	 */
 	public void run(CommonResources resources) throws ModuleException
 	{
-		List<File> toBeHarvested = new ArrayList<File>();
-
-		File dummies = (File) resources.get(CStarJavaCompiler.DUMMY_JAR);
-		try
-		{
-			ClassPathModifier.addFile(dummies);
-			toBeHarvested.add(dummies);
-		}
-		catch (Exception e)
-		{
-			throw new ModuleException("Error while updating classpath" + e.toString(), MODULE_NAME);
-		}
-
-		for (File deps : resources.configuration().getProject().getFilesDependencies())
-		{
-			try
-			{
-				ClassPathModifier.addFile(deps);
-				toBeHarvested.add(deps);
-			}
-			catch (Exception e)
-			{
-				throw new ModuleException("Error while updating classpath" + e.toString(), "HARVESTER");
-			}
-		}
-
+		List<URL> toBeHarvested = new ArrayList<URL>();
 		HashMap<String,Class> classMap = new HashMap<String,Class>();
-		resources.put(JavaHarvestRunner.CLASS_MAP, classMap);
-		
-		for (File file : toBeHarvested)
-		{
-			try
-			{
-				JarLoader jl = new JarLoader(file);
-				HashMap classen = jl.getLoadedClasses();
-				for (Object o : classen.values())
-				{
-					Class c = (Class) o;
-					classMap.put(c.getName(), c);
-					Debug.out(Debug.MODE_DEBUG, "HARVESTER", "Class extracted:" + c.getName());
-				}
-			}
-			catch (JarLoaderException e)
-			{
-				throw new ModuleException("Error while loading classes from " + file + ": " + e.getMessage(),
-						"HARVESTER");
-			}
+		resources.put(JavaHarvestRunner.CLASS_MAP, classMap); // make the classmap accesible to other modules
+
+		try {
+			// Harvest dummy classes
+			toBeHarvested.add(((File) resources.get(CStarJavaCompiler.DUMMY_JAR)).toURL());
+	
+			// As well as all project dependencies
+			for (File deps : resources.configuration().getProject().getFilesDependencies())
+				toBeHarvested.add(deps.toURL());
+
+			// Now harvest these jarFiles for type information
+			for (URL jarFile : toBeHarvested)
+				classMap.putAll(harvest(jarFile));
+		}
+		catch(Exception e) {
+			throw new ModuleException("Error while harvesting types: " + e.getMessage(), "HARVESTER");
 		}
 	}
-
-	/**
-	 * Helper class. A 'hack' to adjust the classpath at runtime.
-	 */
-	private static class ClassPathModifier
-	{
-
-		private static Class[] parameters = new Class[] { URL.class };
-
-		public static void addFile(String s) throws IOException
-		{
-			File f = new File(s);
-			addFile(f);
-		}
-
-		public static void addFile(File f) throws IOException
-		{
-			addURL(f.toURL());
-		}
-
-		public static void addURL(URL u) throws IOException
-		{
-			URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-			Class sysclass = URLClassLoader.class;
-
-			try
-			{
-				Method method = sysclass.getDeclaredMethod("addURL", parameters);
-				method.setAccessible(true);
-				method.invoke(sysloader, new Object[] { u });
-			}
-			catch (Throwable t)
-			{
-				t.printStackTrace();
-				throw new IOException("Error, could not add URL to system classloader");
-			}
-		}
-	}// end class ClassPathModifier
 }// end class JavaHarvestRunner
 
