4279dfe22cec703af84b28518fef91009bc239cb
Michiel Hendriks <git@elmuerte.com>
2008-02-13 08:30:14 +0000

    ported error filter test (this one is broken, see http://sourceforge.net/tracker/index.php?func=detail&aid=1892497&group_id=82370&atid=565849 )

---
+++ StarLight/Examples/TestCases/ErrorFilterTest/Subject.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ErrorFilterTest
+{
+	public class Subject
+	{
+		public Subject()
+		{
+			Console.WriteLine(".ctor");
+		}
+
+		public void f1()
+		{
+			Console.WriteLine("f1");
+		}
+
+		public void f2()
+		{
+			Console.WriteLine("f2");
+		}
+		
+	}
+}
