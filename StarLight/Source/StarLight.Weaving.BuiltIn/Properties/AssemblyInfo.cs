47e5ec6414f79d5293ecf30595e9ae9ba70b5099
Michiel Hendriks <git@elmuerte.com>
2008-04-02 10:06:16 +0000

    version update -> 0.8.2

--- StarLight/Source/StarLight.Weaving.BuiltIn/Properties/AssemblyInfo.cs
+++ StarLight/Source/StarLight.Weaving.BuiltIn/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.8.1.0")]
-[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyVersion("0.8.2.0")]
+[assembly: AssemblyFileVersion("0.8.2.0")]
 [assembly: CLSCompliant(true)]
 
 [assembly: Composestar.StarLight.SkipWeaving()]
