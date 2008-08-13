d6069320cb723547df82de80436806a586dbcce0
Michiel Hendriks <git@elmuerte.com>
2008-08-13 12:47:09 +0000

    cruisecontrol config update changed build number processing fix to the .net installer creation

--- Core/Development/ComposestarAnt/src/Composestar/Ant/Taskdefs/BuildNumberExtractor.java
+++ Core/Development/ComposestarAnt/src/Composestar/Ant/Taskdefs/BuildNumberExtractor.java
@@ -3,55 +3,81 @@
  */
 package Composestar.Ant.Taskdefs;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.tools.ant.Task;
 
 /**
  * @author Michiel Hendriks
- *
  */
 public class BuildNumberExtractor extends Task
-{	
+{
 	protected String label = null;
 
 	protected String property = null;
-	
-	protected String syntax = "build.#";
 
 	/**
 	 * 
 	 */
 	public BuildNumberExtractor()
 	{
-	
-	}
-	
-    /**
-     * Sets the string attribute of the BuildNumberExtractor object
-     *
-     * @param string  The new string value
-     */
-    public void setLabel(String name) {
-        label = name;
-    }
 
-    /**
-     * Sets the property attribute of the BuildNumberExtractor object
-     *
-     * @param name  The new property value
-     */
-    public void setProperty(String name) {
-        property = name;
-    }
-    
-    public void setSyntax(String name) {
-    	syntax = name;
-    }
-    
-    /** Description of the Method */
-    public void execute() {
-    	String before = syntax.substring(0, syntax.indexOf('#'));
-    	String after = syntax.substring(syntax.indexOf('#')+1);    	
-    	String result = label.substring(before.length(), label.length()-after.length());
-    	getProject().setProperty(property, result);
-    }
+	}
+
+	/**
+	 * Sets the string attribute of the BuildNumberExtractor object
+	 * 
+	 * @param string The new string value
+	 */
+	public void setLabel(String name)
+	{
+		label = name;
+	}
+
+	/**
+	 * Sets the property attribute of the BuildNumberExtractor object
+	 * 
+	 * @param name The new property value
+	 */
+	public void setProperty(String name)
+	{
+		property = name;
+	}
+
+	/** Description of the Method */
+	@Override
+	public void execute()
+	{
+		getProject().setProperty(property, getBuildNumber(label));
+	}
+
+	protected String getBuildNumber(String input)
+	{
+		if (input == null || input.length() == 0)
+		{
+			return "0";
+		}
+		Matcher m;
+
+		final Pattern buildpat = Pattern.compile("build\\.([0-9]+)", Pattern.CASE_INSENSITIVE);
+		m = buildpat.matcher(input);
+		if (m.matches())
+		{
+			return m.group(1);
+		}
+
+		final Pattern svnpat = Pattern.compile("svn\\.([0-9]+)(:([0-9]+))?([MSP])?", Pattern.CASE_INSENSITIVE);
+		m = svnpat.matcher(input);
+		if (m.matches())
+		{
+			if (m.group(3) != null)
+			{
+				return m.group(3);
+			}
+			return m.group(1);
+		}
+
+		return "0";
+	}
 }
