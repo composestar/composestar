b0ea5c1ea7b1b5c10b656dda7bf65668492f4143
Michiel Hendriks <git@elmuerte.com>
2009-03-13 09:54:01 +0000

    this should fix the problem in eclipse with refreshing examples during the system test

--- Java/Plugin/Eclipse/src/Composestar/Eclipse/Java/Actions/TestBuild.java
+++ Java/Plugin/Eclipse/src/Composestar/Eclipse/Java/Actions/TestBuild.java
@@ -5,7 +5,9 @@ import java.io.File;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceDescription;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPlatformRunnable;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.IJavaProject;
@@ -48,18 +50,25 @@ public class TestBuild implements IPlatformRunnable
 
 			Debug.instance().setFileLog(new File(project.getLocation().toFile(), "buildlog.txt"));
 
-			project.refreshLocal(IResource.DEPTH_INFINITE, null);
-			// project.build(IncrementalProjectBuilder.FULL_BUILD, npm);
-
-			JavaBuildAction j = new JavaBuildAction();
-			j.setSelectedProjects(projects);
-			j.build();
-
-			Debug.instance().setFileLog(null);
-			if (!j.builtOk)
+			try
 			{
-				return new Integer(1);
+				project.refreshLocal(IResource.DEPTH_INFINITE, npm);
 			}
+			catch (CoreException e)
+			{
+				// just ignore for now
+			}
+			project.build(IncrementalProjectBuilder.FULL_BUILD, npm);
+
+			// JavaBuildAction j = new JavaBuildAction();
+			// j.setSelectedProjects(projects);
+			// j.build();
+			//
+			// Debug.instance().setFileLog(null);
+			// if (!j.builtOk)
+			// {
+			// return new Integer(1);
+			// }
 		}
 		catch (Exception e)
 		{
