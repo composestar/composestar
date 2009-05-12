a5ae52c2543211f842534963de76494dc0f7b47b
Michiel Hendriks <git@elmuerte.com>
2009-05-12 14:58:57 +0000

    increase version number to 0.9.4 for next builds

--- StarLight/Source/ILAnalyzer/Properties/AssemblyInfo.cs
+++ StarLight/Source/ILAnalyzer/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Diagnostics.CodeAnalysis;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.3.0")]
-[assembly: AssemblyFileVersion("0.9.3.0")]
+[assembly: AssemblyVersion("0.9.4.0")]
+[assembly: AssemblyFileVersion("0.9.4.0")]
 [assembly: CLSCompliant(true)]
 
 [module: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Composestar.StarLight.ILAnalyzer")]
