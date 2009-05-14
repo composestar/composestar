c429813655eb2f7fe9a99f70043156baa3d6fd9b
Michiel Hendriks <git@elmuerte.com>
2009-05-14 10:58:50 +0000

    - fixed concurrency issue in the repository serializer

--- Java/Compiletime/src/Composestar/Java/CONE/JavaRepositorySerializer.java
+++ Java/Compiletime/src/Composestar/Java/CONE/JavaRepositorySerializer.java
@@ -6,13 +6,14 @@ import java.io.FileOutputStream;
 import java.io.NotSerializableException;
 import java.io.ObjectOutputStream;
 import java.util.Collection;
-import java.util.Iterator;
 
 import Composestar.Core.CONE.CONE;
 import Composestar.Core.CpsRepository2.Repository;
+import Composestar.Core.CpsRepository2.RepositoryEntity;
 import Composestar.Core.CpsRepository2.References.ReferenceManager;
 import Composestar.Core.CpsRepository2.References.ReferenceUsage;
 import Composestar.Core.CpsRepository2Impl.PrimitiveConcern;
+import Composestar.Core.CpsRepository2Impl.RepositoryImpl;
 import Composestar.Core.Exception.ModuleException;
 import Composestar.Core.Master.ModuleNames;
 import Composestar.Core.Resources.CommonResources;
@@ -39,23 +40,25 @@ public class JavaRepositorySerializer extends CONE
 
 		logger.info("writing repository to file " + repositoryFilename + " ...");
 
-		Repository ds = resources.repository();
-
-		// ds.excludeUnreferenced(PrimitiveConcern.class);
-		logger.debug(String.format("Repository size before: %d", ds.size()));
+		Repository repos = resources.repository();
+		// create a smaller repository for the runtime
+		Repository ds = new RepositoryImpl();
 		ReferenceManager refman = resources.get(ReferenceManager.RESOURCE_KEY);
-		Iterator<PrimitiveConcern> it = ds.getAllIterator(PrimitiveConcern.class);
-		while (it.hasNext())
+		for (RepositoryEntity re : repos)
 		{
-			PrimitiveConcern pc = it.next();
-			Collection<ReferenceUsage> refusers = refman.getReferenceUsage(pc.getTypeReference());
-			if (refusers.isEmpty() && pc.getSuperimposed() == null)
+			if (re instanceof PrimitiveConcern)
 			{
-				logger.trace(String.format("PrimitiveConcern %s not used", pc.getFullyQualifiedName()));
-				it.remove();
+				PrimitiveConcern pc = (PrimitiveConcern) re;
+				Collection<ReferenceUsage> refusers = refman.getReferenceUsage(pc.getTypeReference());
+				if (refusers.isEmpty() && pc.getSuperimposed() == null)
+				{
+					logger.trace(String.format("PrimitiveConcern %s not used", pc.getFullyQualifiedName()));
+					continue;
+				}
 			}
+			ds.add(re);
 		}
-		logger.debug(String.format("Repository size after: %d", ds.size()));
+		logger.debug(String.format("Repository size shrunk to %d from %d", ds.size(), repos.size()));
 
 		ObjectOutputStream oos = null;
 		try
