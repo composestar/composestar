d8ac6a6b3146aeb3e3cc84f26adeb18d23df5d4a
Michiel Hendriks <git@elmuerte.com>
2009-08-05 13:14:59 +0000

    increased version number for future builds

--- StarLight/Source/Repository/Properties/AssemblyInfo.cs
+++ StarLight/Source/Repository/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Diagnostics.CodeAnalysis;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.5.0")]
-[assembly: AssemblyFileVersion("0.9.5.0")]
+[assembly: AssemblyVersion("0.9.6.0")]
+[assembly: AssemblyFileVersion("0.9.6.0")]
 [assembly: CLSCompliant(true)]
 
 [module: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Composestar.Repository")]
